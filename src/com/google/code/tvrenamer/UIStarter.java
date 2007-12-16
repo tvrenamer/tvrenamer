887d0d6948839efa5f3d042c120598412a22800e
vipuldelwadia <vipul.delwadia@gmail.com>
2007-12-16 05:42:07 +0000

    Three changes:  - removed .classpath from project, saves having to commit each time someone changes it!  - added new version of swt-win32.jar, if it doesn't work for you then grab the latest eclipse  - sort table code should be fixed, including keeping properties and checked status

--- src/com/google/code/tvrenamer/UIStarter.java
+++ src/com/google/code/tvrenamer/UIStarter.java
@@ -23,242 +23,269 @@ import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 
 public class UIStarter {
-	private static final int RENAME = 1;
-	private static final int LIST = 2;	
-	private static final String pathSeparator = System.getProperty("file.separator");
-	private static Logger logger = Logger.getLogger(UIStarter.class);
-	
-	private Map<Integer, Episode> episodeListing;
-	private Shell shell = null;
-	private Table tblResults = null;
+  private static final int RENAME = 1;
+  private static final int LIST = 2;
+  private static final String pathSeparator = System
+      .getProperty("file.separator");
+  private static Logger logger = Logger.getLogger(UIStarter.class);
 
-	public static void main(String[] args) {
-		UIStarter ui = new UIStarter();
-		ui.init();
-		ui.launch();
-	}
-	
-	private Shell init() {
-		// Set up environment
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 3;
-		final Display display = new Display();
-		shell = new Shell(display);
-		shell.setText("TVRenamer");
-		shell.setLayout(gridLayout);
+  private Map<Integer, Episode> episodeListing;
+  private Shell shell = null;
+  private Table tblResults = null;
 
-		//File browsing
-		final FileDialog fd = new FileDialog(shell, SWT.MULTI);
-		Button btnBrowse = new Button(shell, SWT.PUSH);
-		btnBrowse.setText("Browse files...");
-		GridData gridData = new GridData();
-		gridData.horizontalSpan = 3;
-		btnBrowse.setLayoutData(gridData);
-		
-		//Results table
-		tblResults = new Table(shell, SWT.CHECK);
-		tblResults.setHeaderVisible(true);
-		tblResults.setLinesVisible(true);
-		gridData = new GridData(GridData.FILL_BOTH);
-		gridData.widthHint = 780;
-		gridData.heightHint = 80;
-		gridData.horizontalSpan = 3;
-		tblResults.setLayoutData(gridData);
-		
-		final TableColumn col1 = new TableColumn(tblResults, SWT.LEFT);
-		col1.setText("Episode");
-		col1.setWidth(80);
-		
-		final TableColumn col2 = new TableColumn(tblResults,SWT.LEFT);
-		col2.setText("Old Name");
-		col2.setWidth(350);
-		
-		final TableColumn col3 = new TableColumn(tblResults, SWT.LEFT);
-		col3.setText("New Name");
-		col3.setWidth(350);
-		
-		final Button btnRenameAll = new Button(shell, SWT.PUSH);
-		btnRenameAll.setText("Rename All");
+  public static void main(String[] args) {
+    UIStarter ui = new UIStarter();
+    ui.init();
+    ui.launch();
+  }
 
-		final Button btnRenameSelected = new Button(shell, SWT.PUSH);
-		btnRenameSelected.setText("Rename Selected");
+  private Shell init() {
+    // Set up environment
+    GridLayout gridLayout = new GridLayout();
+    gridLayout.numColumns = 3;
+    final Display display = new Display();
+    shell = new Shell(display);
+    shell.setText("TVRenamer");
+    shell.setLayout(gridLayout);
 
-		final Button btnQuit = new Button(shell, SWT.PUSH);
-		btnQuit.setText("Quit");
-		gridData = new GridData(GridData.HORIZONTAL_ALIGN_END);
-		btnQuit.setLayoutData(gridData);
-		
-		
-		btnBrowse.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent  e) {
-				//Clear the table for new use
-				tblResults.removeAll();
+    // File browsing
+    final FileDialog fd = new FileDialog(shell, SWT.MULTI);
+    Button btnBrowse = new Button(shell, SWT.PUSH);
+    btnBrowse.setText("Browse files...");
+    GridData gridData = new GridData();
+    gridData.horizontalSpan = 3;
+    btnBrowse.setLayoutData(gridData);
 
-				String pathPrefix = fd.open();
-				if(pathPrefix != null) {
-					File file = new File(pathPrefix);
-					pathPrefix = file.getParent();
-					
-					String [] fileNames = fd.getFileNames();					
+    // Results table
+    tblResults = new Table(shell, SWT.CHECK);
+    tblResults.setHeaderVisible(true);
+    tblResults.setLinesVisible(true);
+    gridData = new GridData(GridData.FILL_BOTH);
+    gridData.widthHint = 780;
+    gridData.heightHint = 80;
+    gridData.horizontalSpan = 3;
+    tblResults.setLayoutData(gridData);
 
-					for (int i = 0; i < fileNames.length; i++) {
-						fileNames[i] = pathPrefix + pathSeparator + fileNames[i];				
-					}
-					
-					logger.debug(fileNames.length + " files successfully added to list");
-					
-					//Call tvRenamer which actually finds the ep names ...
-					TVRenamer tv = new TVRenamer(fileNames);
+    final TableColumn col1 = new TableColumn(tblResults, SWT.LEFT);
+    col1.setText("Episode");
+    col1.setWidth(80);
 
-					//... and get the resuts in a map
-					episodeListing = tv.getEpisodeListing();
-					renameFiles(LIST);
-					
-					//Sort the list descending by Episode
-					tblResults.setSortColumn(col1);
-					tblResults.setSortDirection(SWT.DOWN);
-					//sortTable(col1, 1);
-				}
-			}
-		});
+    final TableColumn col2 = new TableColumn(tblResults, SWT.LEFT);
+    col2.setText("Old Name");
+    col2.setWidth(350);
 
-		btnRenameAll.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent  e) {
-				renameFiles(RENAME);
-			}
-		});
-		
-		col1.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent  e) {
-				tblResults.setSortColumn(col1);
-				tblResults.setSortDirection(tblResults.getSortDirection() == SWT.DOWN ? SWT.UP : SWT.DOWN);
-				sortTable(col1, 1);
-			}
-		});
-		
-		col2.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent  e) {
-				tblResults.setSortColumn(col2);
-				tblResults.setSortDirection(tblResults.getSortDirection() == SWT.DOWN ? SWT.UP : SWT.DOWN);
-				sortTable(col2, 2);
-			}
-		});
-		
-		col3.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent  e) {
-				tblResults.setSortColumn(col3);
-				tblResults.setSortDirection(tblResults.getSortDirection() == SWT.DOWN ? SWT.UP : SWT.DOWN);
-				sortTable(col2, 2);
-			}
-		});
+    final TableColumn col3 = new TableColumn(tblResults, SWT.LEFT);
+    col3.setText("New Name");
+    col3.setWidth(350);
 
-		btnRenameSelected.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent  e) {				
-				//Remove the unticked entries from the map
-				TableItem[] items = tblResults.getItems();
-				for(int i=0; i<items.length; i++) {
-					if(!items[i].getChecked()) {
-						episodeListing.remove(Integer.parseInt(items[i].getText()));
-					}
-				}				
-				renameFiles(RENAME);				
-			}
-		});
+    final Button btnRenameAll = new Button(shell, SWT.PUSH);
+    btnRenameAll.setText("Rename All");
 
-		btnQuit.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent  e) {
-				display.dispose();
-			}
-		});
-		
-		return shell;
-	}
-	
-	private void launch() {
-		// Start the shell
-		shell.pack();
-		shell.open();
-		
-		Display display = shell.getDisplay();
-		while(!shell.isDisposed()) {
-			if (!display.readAndDispatch())
-				display.sleep();
-		}
-		display.dispose();
-	}
-			
-	private void renameFiles(int action) {
-		int renamedFiles = 0;
-		for(Episode ep : episodeListing.values()) {			
-			File file = new File(ep.getPath());
-			String parentFilename = file.getParent() + pathSeparator;
-			String extension = getExtension(file);
-			String newFilename = ep.getShow() + " [" + ep.getSeason() + "x" + ep.getNumber() +"] " + ep.getTitle() + "." + extension;
-			
-			if(action == LIST) {
-				//Add entry to table
-				TableItem item = new TableItem(tblResults, SWT.NONE);
-				item.setText(new String[]{ep.getNumber(), file.getName(), newFilename});
-				item.setChecked(true);
-			}
-			
-			else {
-				file.renameTo(new File(parentFilename + newFilename));
-				logger.info("Sucessfully renamed \'" + file.getName() + "\' to \'" + newFilename + "\'");
-				renamedFiles++;
-			}
-		}
-		if(renamedFiles > 0) {			
-			MessageBox msgSuccess = new MessageBox(shell, SWT.OK|SWT.ICON_INFORMATION);
-			msgSuccess.setMessage(renamedFiles + " files successfully renamed!");
-			msgSuccess.open();
-		}
-	}
-	
-	private String getExtension(File file) {
-		String fileName = file.getName();
-		String regex = ".*\\.(.*)";
-		
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(fileName);
-		
-		String extn = "";
-		if(matcher.matches()) {
-			extn = matcher.group(1);
-		}
-		return extn;
-	}
+    final Button btnRenameSelected = new Button(shell, SWT.PUSH);
+    btnRenameSelected.setText("Rename Selected");
 
-	private void sortTable(TableColumn col, int position) {
-		//Get the items
-        TableItem[] items = tblResults.getItems();
-        Collator collator = Collator.getInstance(Locale.getDefault());
-        
-        //Go through the item list and 
-        for (int i = 1; i < items.length; i++) {
-            String value1 = items[i].getText(position);
-            for (int j = 0; j < i; j++){
-                String value2 = items[j].getText(position);
-                //Compare the two values and order accordingly
-                if(tblResults.getSortDirection() == SWT.DOWN) {
-	                if (collator.compare(value1, value2) < 0) {
-	                    String[] values = {items[i].getText(0), items[i].getText(1), items[i].getText(2)};
-	                    items[i].dispose();
-	                    TableItem item = new TableItem(tblResults, SWT.NONE, j);
-	                    item.setText(values);
-	                    break;
-	                }
-                }
-	            else {
-	                if (collator.compare(value1, value2) > 0){
-	                    String[] values = {items[i].getText(0), items[i].getText(1), items[i].getText(2)};
-	                    items[i].dispose();
-	                    TableItem item = new TableItem(tblResults, SWT.NONE, j);
-	                    item.setText(values);
-	                    break;
-	                }
-	            }
-            }
+    final Button btnQuit = new Button(shell, SWT.PUSH);
+    btnQuit.setText("Quit");
+    gridData = new GridData(GridData.HORIZONTAL_ALIGN_END);
+    btnQuit.setLayoutData(gridData);
+
+    btnBrowse.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        // Clear the table for new use
+        tblResults.removeAll();
+
+        String pathPrefix = fd.open();
+        if (pathPrefix != null) {
+          File file = new File(pathPrefix);
+          pathPrefix = file.getParent();
+
+          String[] fileNames = fd.getFileNames();
+
+          for (int i = 0; i < fileNames.length; i++) {
+            fileNames[i] = pathPrefix + pathSeparator + fileNames[i];
+          }
+
+          logger.debug(fileNames.length + " files successfully added to list");
+
+          // Call tvRenamer which actually finds the ep names ...
+          TVRenamer tv = new TVRenamer(fileNames);
+
+          // ... and get the resuts in a map
+          episodeListing = tv.getEpisodeListing();
+          renameFiles(LIST);
+
+          // Sort the list descending by Episode
+          tblResults.setSortColumn(col1);
+          tblResults.setSortDirection(SWT.DOWN);
+          // sortTable(col1, 1);
         }
-	}
+      }
+    });
+
+    btnRenameAll.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        renameFiles(RENAME);
+      }
+    });
+
+    col1.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        tblResults
+            .setSortDirection(tblResults.getSortDirection() == SWT.DOWN ? SWT.UP
+                : SWT.DOWN);
+        sortTable(col1, 1);
+        tblResults.setSortColumn(col1);
+      }
+    });
+
+    col2.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        tblResults
+            .setSortDirection(tblResults.getSortDirection() == SWT.DOWN ? SWT.UP
+                : SWT.DOWN);
+        sortTable(col2, 2);
+        tblResults.setSortColumn(col2);
+      }
+    });
+
+    col3.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        tblResults
+            .setSortDirection(tblResults.getSortDirection() == SWT.DOWN ? SWT.UP
+                : SWT.DOWN);
+        sortTable(col3, 1);
+        tblResults.setSortColumn(col3);
+      }
+    });
+
+    btnRenameSelected.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        // Remove the unticked entries from the map
+        TableItem[] items = tblResults.getItems();
+        for (int i = 0; i < items.length; i++) {
+          if (!items[i].getChecked()) {
+            episodeListing.remove(Integer.parseInt(items[i].getText()));
+          }
+        }
+        renameFiles(RENAME);
+      }
+    });
+
+    btnQuit.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        display.dispose();
+      }
+    });
+
+    return shell;
+  }
+
+  private void launch() {
+    // Start the shell
+    shell.pack();
+    shell.open();
+
+    Display display = shell.getDisplay();
+    while (!shell.isDisposed()) {
+      if (!display.readAndDispatch())
+        display.sleep();
+    }
+    display.dispose();
+  }
+
+  private void renameFiles(int action) {
+    int renamedFiles = 0;
+    for (Episode ep : episodeListing.values()) {
+      File file = new File(ep.getPath());
+      String parentFilename = file.getParent() + pathSeparator;
+      String extension = getExtension(file);
+      String newFilename = ep.getShow() + " [" + ep.getSeason() + "x"
+          + ep.getNumber() + "] " + ep.getTitle() + "." + extension;
+
+      if (action == LIST) {
+        // Add entry to table
+        TableItem item = new TableItem(tblResults, SWT.NONE);
+        item
+            .setText(new String[] { ep.getNumber(), file.getName(), newFilename });
+        item.setChecked(true);
+      }
+
+      else {
+        file.renameTo(new File(parentFilename + newFilename));
+        logger.info("Sucessfully renamed \'" + file.getName() + "\' to \'"
+            + newFilename + "\'");
+        renamedFiles++;
+      }
+    }
+    if (renamedFiles > 0) {
+      MessageBox msgSuccess = new MessageBox(shell, SWT.OK
+          | SWT.ICON_INFORMATION);
+      msgSuccess.setMessage(renamedFiles + " files successfully renamed!");
+      msgSuccess.open();
+    }
+  }
+
+  private String getExtension(File file) {
+    String fileName = file.getName();
+    String regex = ".*\\.(.*)";
+
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(fileName);
+
+    String extn = "";
+    if (matcher.matches()) {
+      extn = matcher.group(1);
+    }
+    return extn;
+  }
+
+  private void sortTable(TableColumn col, int position) {
+    // Get the items
+    TableItem[] items = tblResults.getItems();
+    Collator collator = Collator.getInstance(Locale.getDefault());
+
+    // Go through the item list and
+    for (int i = 1; i < items.length; i++) {
+      String value1 = items[i].getText(position);
+      for (int j = 0; j < i; j++) {
+        String value2 = items[j].getText(position);
+        // Compare the two values and order accordingly
+        if (tblResults.getSortDirection() == SWT.DOWN) {
+          if (collator.compare(value1, value2) < 0) {
+            setSortedItem(i, j);
+            // the snippet replaces the items with the new items, we do the same
+            items = tblResults.getItems();
+            break;
+          }
+        } else {
+          if (collator.compare(value1, value2) > 0) {
+            setSortedItem(i, j);
+            // the snippet replaces the items with the new items, we do the same
+            items = tblResults.getItems();
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  private void setSortedItem(int i, int j) {
+    TableItem oldItem = tblResults.getItem(i);
+    boolean wasChecked = oldItem.getChecked();
+    int oldStyle = oldItem.getStyle();
+    String[] values = { oldItem.getText(0), oldItem.getText(1),
+        oldItem.getText(2) };
+    oldItem.dispose();
+    TableItem item = new TableItem(tblResults, oldStyle, j);
+    item.setText(values);
+    item.setChecked(wasChecked);
+
+  }
 }
