d0647f8be3f0a54961a564bd922b29f0cad6913a
Dave Harris <dave@nzmac.com>
2010-09-06 14:15:29 +1200

    Completed logging infrastructure

---
+++ src/main/com/google/code/tvrenamer/model/util/StdOutConsoleFormatter.java
@@ -0,0 +1,44 @@
+package com.google.code.tvrenamer.model.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+public class StdOutConsoleFormatter extends Formatter {
+
+  @Override
+  public String format(LogRecord rec) {
+    StringBuffer buffer = new StringBuffer(1000);
+    
+    // Date
+    String formatString = "[dd/MM/yy kk:mm:ss,SS] ";
+    SimpleDateFormat sdf = new SimpleDateFormat(formatString);
+    Date date = new Date(rec.getMillis());
+    buffer.append(sdf.format(date));
+    
+    // Level
+    if(rec.getLevel() == Level.WARNING) {
+      buffer.append("WARN ");
+    } else {
+      buffer.append(rec.getLevel() + " ");
+    }
+    
+    // Class, method
+    buffer.append(rec.getSourceClassName() + "#");
+    buffer.append(rec.getSourceMethodName() + " ");
+    
+    // Message
+    buffer.append(rec.getMessage());
+    
+    // Stacktrace
+    if(rec.getThrown() != null) {
+      buffer.append("\n" + rec.getThrown().getStackTrace());
+    }
+    
+    // Note: No need to add a newline as that is added by the Handler
+    
+    return buffer.toString();
+  }
+}
---
+++ src/main/com/google/code/tvrenamer/model/util/StdOutConsoleHandler.java
@@ -0,0 +1,42 @@
+package com.google.code.tvrenamer.model.util;
+
+import java.io.OutputStream;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+import java.util.logging.StreamHandler;
+
+/**
+ * 
+ * @author Dave Harris
+ */
+public class StdOutConsoleHandler extends StreamHandler {
+  
+  private Formatter formatter = getFormatter();
+
+  @Override
+  public synchronized void publish(LogRecord logRecord) {
+    // Write the formatted log record to stdout
+    System.out.println(formatter.format(logRecord));
+  }
+  
+  @Override
+  public synchronized void close() throws SecurityException {
+    // We don't want the logger to close the stdout stream
+  }
+
+  @Override
+  public synchronized void flush() {
+    super.flush();
+  }
+
+  @Override
+  public boolean isLoggable(LogRecord record) {
+    return true;
+  }
+
+  @Override
+  protected synchronized void setOutputStream(OutputStream arg0) throws SecurityException {
+    super.setOutputStream(arg0);
+  }
+
+}
