8e4740a1295ffa842302c63e5391b57caab01442
Vipul Delwadia <vipul.delwadia@coretech.co.nz>
2010-11-13 16:16:03 +1300

    Added shell script to create a bunch of empty files on disk; helps with testing.

---
+++ etc/create-test-files.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+echo "24.S07.E18.720p.BlueRay.x264-SiNNERS.mkv
+24.s08.e01.720p.hdtv.x264-immerse.mkv
+JAG.S10E01.DVDRip.XviD-P0W4DVD.avi
+Lost.S06E05.Lighthouse.DD51.720p.WEB-DL.AVC-FUSiON.mkv
+castle.2009.s01e09.720p.hdtv.x264-ctu.mkv
+dexter.407.720p.hdtv.x264-sys.mkv
+gossip.girl.s03e15.hdtv.xvid-fqm.avi
+human.target.2010.s01.e02.720p.hdtv.x264-2hd.mkv
+one.tree.hill.s07e14.hdtv.xvid-fqm.avi
+smallville.s09e14.hdtv.xvid-xii.avi
+smallville.s09e15.hdtv.xvid-2hd.avi
+the.big.bang.theory.s03e18.720p.hdtv.x264-ctu.mkv
+warehouse.13.s1e01.720p.hdtv.x264-dimension.mkv" | 
+while read line;
+	do touch $line;
+done
