tvrenamer/tvrenamer

commit d0d2daf50f1b26a061d504f4a34480c1b7401e74

Little code fixup

src/main/com/google/code/tvrenamer/model/UserPreferences.java
@@ -246,9 +246,6 @@ public class UserPreferences extends Observable {
 	}
 
 	private boolean hasChanged(Object originalValue, Object newValue) {
-		if (originalValue.equals(newValue)) {
-			return false;
-		}
-		return true;
+		return originalValue.equals(newValue);
 	}
 }

commit d91d8db99fab17bdf3a1493c33a366d48bf6a328

Fix .hasChanged() returning inverted value
Broken by commit d0d2daf50f

src/main/com/google/code/tvrenamer/model/UserPreferences.java
@@ -265,6 +265,6 @@ public class UserPreferences extends Observable {
 	}
 
 	private boolean hasChanged(Object originalValue, Object newValue) {
-		return originalValue.equals(newValue);
+		return !originalValue.equals(newValue);
 	}
 }