5ec1bc86b96b3ff4b9afa3d62ea0e40f3203d68b
Vipul Delwadia <vipul.delwadia@gmail.com>
2014-05-20 11:17:44 +1200

    Made the ShowStore concurrency less lock-y:
     - removed the semaphore and changed the underlying show store Map to be
       a SynchronizedMap which will handle the concurrency instead
     - performance seems to be much better for multiple show renames, since
       the threads aren't waiting for a lock anymore

--- src/main/com/google/code/tvrenamer/controller/UserPreferencesChangeListener.java
+++ src/main/com/google/code/tvrenamer/controller/UserPreferencesChangeListener.java
@@ -2,7 +2,6 @@ package com.google.code.tvrenamer.controller;
 
 import java.util.Observable;
 import java.util.Observer;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.google.code.tvrenamer.model.ShowStore;
@@ -18,22 +17,18 @@ public class UserPreferencesChangeListener implements Observer {
 	@Override
 	public void update(Observable observable, Object value) {
 		logger.info("Preference change event: " + value);
-		
-		if(observable instanceof UserPreferences && value instanceof UserPreferencesChangeEvent) {
+
+		if (observable instanceof UserPreferences && value instanceof UserPreferencesChangeEvent) {
 			UserPreferencesChangeEvent upce = (UserPreferencesChangeEvent) value;
-			
-			if(upce.getPreference().equals("moveEnabled")) {
+
+			if (upce.getPreference().equals("moveEnabled")) {
 				UIStarter.setRenameButtonText();
 				UIStarter.setColumnDestText();
 			}
-			
-			if(upce.getPreference().equals("proxy")) {
+
+			if (upce.getPreference().equals("proxy")) {
 				// There may be incorrect entries in ShowStore if there is no internet, so clear on proxy change
-				try {
-					ShowStore.clear();
-				} catch (InterruptedException e) {
-					logger.log(Level.SEVERE, "Error when attempting to clear store Map", e);
-				}
+				ShowStore.clear();
 			}
 		}
 	}
