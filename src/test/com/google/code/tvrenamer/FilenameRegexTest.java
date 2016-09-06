0c6515cec61059a147bffd2aef3448cd23da44c8
John Valente <johnv02139@yahoo.com>
2016-09-06 15:23:28 -0700

    Add tests for replacePunctuation and sanitiseTitle.

--- src/test/com/google/code/tvrenamer/FilenameRegexTest.java
+++ src/test/com/google/code/tvrenamer/FilenameRegexTest.java
@@ -1,5 +1,7 @@
 package com.google.code.tvrenamer;
 
+import static org.junit.Assert.assertEquals;
+
 import java.text.DecimalFormat;
 import java.util.regex.Matcher;
 
@@ -40,4 +42,44 @@ public class FilenameRegexTest {
 			}
 		}
 	}
+
+  @Test
+  public void testSanitizeTitle() {
+    assertEquals("Microsoft - Windows", StringUtils.sanitiseTitle("Microsoft / Windows"));
+    assertEquals("Microsoft - Windows", StringUtils.sanitiseTitle("Microsoft | Windows"));
+    assertEquals("Microsoft - Windows", StringUtils.sanitiseTitle("Microsoft \\ Windows"));
+    assertEquals("Microsoft  - Windows", StringUtils.sanitiseTitle("Microsoft : Windows"));
+    assertEquals("Microsoft ' Windows", StringUtils.sanitiseTitle("Microsoft ` Windows"));
+    assertEquals("Microsoft ' Windows", StringUtils.sanitiseTitle("Microsoft \" Windows"));
+    assertEquals("Microsoft  Windows", StringUtils.sanitiseTitle("Microsoft * Windows"));
+    assertEquals("Microsoft  Windows", StringUtils.sanitiseTitle("Microsoft ? Windows"));
+    assertEquals("Microsoft  Windows", StringUtils.sanitiseTitle("Microsoft < Windows"));
+    assertEquals("Microsoft  Windows", StringUtils.sanitiseTitle("Microsoft > Windows"));
+  }
+
+  @Test
+  public void testReplacePunctuation() {
+    assertEquals("Marvels Agents of SHIELD", StringUtils.replacePunctuation("Marvel's Agents of S.H.I.E.L.D."));
+    assertEquals("Star Trek The Next Generation", StringUtils.replacePunctuation("Star Trek: The Next Generation"));
+    assertEquals("Monty Pythons Flying Circus", StringUtils.replacePunctuation("Monty Python's Flying Circus"));
+    assertEquals("Married with Children", StringUtils.replacePunctuation("Married... with Children"));
+    assertEquals("God The Devil and Bob", StringUtils.replacePunctuation("God, The Devil and Bob"));
+    assertEquals("Whats Happening", StringUtils.replacePunctuation("What's Happening!!"));
+    assertEquals("Brooklyn Nine Nine", StringUtils.replacePunctuation("Brooklyn Nine-Nine"));
+    assertEquals("Murder She Wrote", StringUtils.replacePunctuation("Murder, She Wrote"));
+    assertEquals("Andy Barker PI", StringUtils.replacePunctuation("Andy Barker, P.I."));
+    assertEquals("Sit Down Shut Up", StringUtils.replacePunctuation("Sit Down, Shut Up"));
+    assertEquals("The Real ONeals", StringUtils.replacePunctuation("The Real O'Neals"));
+    assertEquals("The Office US", StringUtils.replacePunctuation("The Office (US)"));
+    assertEquals("That 70s Show", StringUtils.replacePunctuation("That '70s Show"));
+    assertEquals("Eerie Indiana", StringUtils.replacePunctuation("Eerie, Indiana"));
+    assertEquals("American Dad", StringUtils.replacePunctuation("American Dad!"));
+    assertEquals("Bobs Burgers", StringUtils.replacePunctuation("Bob's Burgers"));
+    assertEquals("Man vs Wild", StringUtils.replacePunctuation("Man vs. Wild"));
+    assertEquals("The X Files", StringUtils.replacePunctuation("The X-Files"));
+    assertEquals("Myth Busters", StringUtils.replacePunctuation("MythBusters"));
+    assertEquals("Blackish", StringUtils.replacePunctuation("Black-ish"));
+    assertEquals("Mr Robot", StringUtils.replacePunctuation("Mr. Robot"));
+    assertEquals("Starving", StringUtils.replacePunctuation("Star-ving"));
+  }
 }
