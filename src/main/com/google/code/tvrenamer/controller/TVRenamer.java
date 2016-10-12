adfc237183d931a2c6cf5bdd04212cf3513d6714
lsrom <lukas.srom@gmail.com>
2016-10-12 16:51:21 +0200

    Perform replacement TVRenamer.java
    Updating com.google.code.tvrenamer.controller.TVRenamer.java regex and matching.
    
     Changes to be committed:
    	modified:   src/main/com/google/code/tvrenamer/controller/TVRenamer.java

--- src/main/com/google/code/tvrenamer/controller/TVRenamer.java
+++ src/main/com/google/code/tvrenamer/controller/TVRenamer.java
@@ -11,19 +11,23 @@ import com.google.code.tvrenamer.model.FileEpisode;
 public class TVRenamer {
 	private static Logger logger = Logger.getLogger(TVRenamer.class.getName());
 
-    public static final String[] REGEX = {
-		"(.+?\\d{4}\\W\\D*?)[sS]?(\\d\\d?)\\D*?(\\d\\d).*", // this one works for titles with years
-		"(.+?\\W\\D*?)[sS](\\d\\d?)[eE](\\d\\d?).*", // this one matches SXXEXX
-		"(.+\\W\\D*?)[sS](\\d\\d?)\\D*?[eE](\\d\\d).*", // this one matches sXX.eXX
-		"(.+\\W\\D*?)(\\d\\d?)\\D+(\\d\\d).*", // this one matches everything else
-		"(.+\\W+)(\\d\\d?)(\\d\\d).*" // truly last resort
-};
+	public static final String[] REGEX = {
+			"(.+?\\d{4}\\W\\D*?)[sS]?(\\d\\d?)\\D*?(\\d\\d).*\\D(\\d+[pk]).*", // this one works for titles with years
+			"(.+?\\W\\D*?)[sS](\\d\\d?)[eE](\\d\\d?).*\\D(\\d+[pk]).*", // this one matches SXXEXX
+			"(.+\\W\\D*?)[sS](\\d\\d?)\\D*?[eE](\\d\\d).*\\D(\\d+[pk]).*", // this one matches sXX.eXX
+			"(.+\\W\\D*?)(\\d\\d?)\\D+(\\d\\d).*\\D(\\d+[pk]).*", // this one matches everything else
+			"(.+\\W+)(\\d\\d?)(\\d\\d).*\\D(\\d+[pk]).*" // truly last resort
+	};
 
 	public static final Pattern[] COMPILED_REGEX = new Pattern[REGEX.length];
 
 	static {
-		for (int i = 0; i < REGEX.length; i++) {
-			COMPILED_REGEX[i] = Pattern.compile(REGEX[i]);
+		for (int i = 0; i < REGEX.length * 2; i++) {
+			if (i / REGEX.length == 0){
+				COMPILED_REGEX[i] = Pattern.compile(REGEX[i]);
+			} else {
+				COMPILED_REGEX[i] = Pattern.compile(REGEX[i - REGEX.length].replace(".*\\D(\\d+[pk])", ""));
+			}
 		}
 	}
 
@@ -38,14 +42,25 @@ public class TVRenamer {
 		Matcher matcher = null;
 		while (idx < COMPILED_REGEX.length) {
 			matcher = COMPILED_REGEX[idx++].matcher(fName);
-			if (matcher.matches() && matcher.groupCount() == 3) {
+			if (matcher.matches() && matcher.groupCount() == 4) {
 				String show = matcher.group(1);
 				show = StringUtils.replacePunctuation(show).trim().toLowerCase();
 
 				int season = Integer.parseInt(matcher.group(2));
 				int episode = Integer.parseInt(matcher.group(3));
+				String resolution = matcher.group(4);
 
-				FileEpisode ep = new FileEpisode(show, season, episode, f);
+				FileEpisode ep = new FileEpisode(show, season, episode, resolution, f);
+				return ep;
+			} else if (matcher.matches() && matcher.groupCount() == 3){
+				String show = matcher.group(1);
+				show = StringUtils.replacePunctuation(show).trim().toLowerCase();
+
+				int season = Integer.parseInt(matcher.group(2));
+				int episode = Integer.parseInt(matcher.group(3));
+				String resolution = "";
+
+				FileEpisode ep = new FileEpisode(show, season, episode, resolution, f);
 				return ep;
 			}
 		}
@@ -57,8 +72,6 @@ public class TVRenamer {
 		String output = input;
 		output = removeLast(output, "hdtv");
 		output = removeLast(output, "dvdrip");
-		output = removeLast(output, "720p");
-		output = removeLast(output, "1080p");
 		return output;
 
 	}
