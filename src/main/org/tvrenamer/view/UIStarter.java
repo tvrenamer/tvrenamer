e15dc4476a5a485bb9e49fc42b2a1e949751972c
Episode Renamer <eprenamer@email.com>
2017-02-28 22:04:24 -0800

    Update ignoreKeywords when notified.

--- src/main/org/tvrenamer/view/UIStarter.java
+++ src/main/org/tvrenamer/view/UIStarter.java
@@ -1046,6 +1046,10 @@ public class UIStarter implements Observer {
         {
             refreshTable();
         }
+
+        if (upref == UserPreference.IGNORE_REGEX) {
+            ignoreKeywords = observed.getIgnoreKeywords();
+        }
     }
 
     /* (non-Javadoc)
