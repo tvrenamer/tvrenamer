c4073247ac2e54a889dc8330727e934573fc2c17
Episode Renamer <eprenameremail.com>
2017-03-19 02:14:24 -0700

    Rework how we replace spaces with dots in titles.

--- src/test/org/tvrenamer/controller/util/StringUtilsTest.java
+++ src/test/org/tvrenamer/controller/util/StringUtilsTest.java
@@ -65,6 +65,57 @@ public class StringUtilsTest {
     }
 
     @Test
+    public void testDotTitle() {
+        assertEquals("#HappyHolograms", StringUtils.makeDotTitle("#HappyHolograms"));
+        assertEquals("'Twas.the.Nightmare.Before.Christmas",
+                     StringUtils.makeDotTitle("'Twas the Nightmare Before Christmas"));
+        assertEquals("1%", StringUtils.makeDotTitle("1%"));
+        assertEquals("200(1)", StringUtils.makeDotTitle("200 (1)"));
+
+        assertEquals("Helen.Keller!The.Musical", StringUtils.makeDotTitle("Helen Keller! The Musical"));
+
+        assertEquals("And.in.Case.I.Don't.See.Ya", StringUtils.makeDotTitle("And in Case I Don't See Ya"));
+
+        assertEquals("Are.You.There.God.It's.Me,Jesus", StringUtils.makeDotTitle("Are You There God It's Me, Jesus"));
+
+        assertEquals("The.Return.of.Dorothy's.Ex(a.k.a.Stan's.Return)",
+                     StringUtils.makeDotTitle("The Return of Dorothy's Ex (a.k.a. Stan's Return)"));
+        assertEquals("Girls.Just.Wanna.Have.Fun...Before.They.Die",
+                     StringUtils.makeDotTitle("Girls Just Wanna Have Fun...Before They Die"));
+        assertEquals("Terrance&Phillip.in'Not.Without.My.Anus'",
+                     StringUtils.makeDotTitle("Terrance & Phillip in 'Not Without My Anus'"));
+
+        assertEquals("B&B's.B'n.B", StringUtils.makeDotTitle("B & B's B'n B"));
+
+        assertEquals("AWESOM-O", StringUtils.makeDotTitle("AWESOM-O"));
+        assertEquals("Coon.2-Hindsight(1)", StringUtils.makeDotTitle("Coon 2 - Hindsight (1)"));
+        assertEquals("Class.Pre-Union", StringUtils.makeDotTitle("Class Pre-Union"));
+        assertEquals("D-Yikes!", StringUtils.makeDotTitle("D-Yikes!"));
+        assertEquals("Ebbtide.VI-The.Wrath.of.Stan",
+                     StringUtils.makeDotTitle("Ebbtide VI - The Wrath of Stan"));
+        assertEquals("Goth.Kids.3-Dawn.of.the.Posers",
+                     StringUtils.makeDotTitle("Goth Kids 3 - Dawn of the Posers"));
+        assertEquals("Jerry-Portrait.of.a.Video.Junkie",
+                     StringUtils.makeDotTitle("Jerry - Portrait of a Video Junkie"));
+        assertEquals("Musso-a.Wedding", StringUtils.makeDotTitle("Musso - a Wedding"));
+        assertEquals("Poetic.License-An.Ode.to.Holden.Caulfield",
+                     StringUtils.makeDotTitle("Poetic License - An Ode to Holden Caulfield"));
+        assertEquals("Sixteen.Candles.and.400-lb.Men",
+                     StringUtils.makeDotTitle("Sixteen Candles and 400-lb. Men"));
+        assertEquals("Slapsgiving.2-Revenge.of.the.Slap",
+                     StringUtils.makeDotTitle("Slapsgiving 2 - Revenge of the Slap"));
+        assertEquals("Valentine's.Day.4-Twisted.Sister",
+                     StringUtils.makeDotTitle("Valentine's Day 4 - Twisted Sister"));
+
+        assertEquals("Ro\\$e.Love\\$Mile\\$", StringUtils.makeDotTitle("Ro\\$e Love\\$ Mile\\$"));
+
+        assertEquals("Believe.it.or.Not,Joe's.Walking.on.Air",
+                     StringUtils.makeDotTitle("Believe it or Not, Joe's Walking on Air"));
+        assertEquals("Eek,A.Penis!", StringUtils.makeDotTitle("Eek, A Penis!"));
+        assertEquals("I.Love.You,Donna.Karan(1)", StringUtils.makeDotTitle("I Love You, Donna Karan (1)"));
+    }
+
+    @Test
     public void testReplacePunctuation() {
         assertEquals("Marvels Agents of SHIELD", StringUtils.replacePunctuation("Marvel's Agents of S.H.I.E.L.D."));
         assertEquals("Star Trek The Next Generation", StringUtils.replacePunctuation("Star Trek: The Next Generation"));
