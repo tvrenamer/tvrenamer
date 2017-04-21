538469c94d4da4fa972f905459ad64c352cb1b8f
Episode Renamer <eprenamer@email.com>
2017-04-21 10:28:41 -0700

    Make use of Constants in FileEpisode.

--- src/main/org/tvrenamer/model/util/Constants.java
+++ src/main/org/tvrenamer/model/util/Constants.java
@@ -53,6 +53,8 @@ public class Constants {
     public static final String ERROR_LABEL = "Error";
 
     public static final String ERROR_PARSING_XML = "Error parsing XML";
+    public static final String ADDED_PLACEHOLDER_FILENAME = "Downloading ...";
+    public static final String BROKEN_PLACEHOLDER_FILENAME = "Unable to download show information";
 
     public static final String DEFAULT_REPLACEMENT_MASK = "%S [%sx%0e] %t";
     public static final String DEFAULT_SEASON_PREFIX = "Season ";
