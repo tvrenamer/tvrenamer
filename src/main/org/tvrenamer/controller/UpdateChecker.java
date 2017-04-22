758c21e1d8d047a493fe5bd905262db6e63655df
Episode Renamer <eprenamer@email.com>
2017-04-21 18:13:44 -0700

    Use import static in AboutDialog and UpdateChecker
    Use values from Constants.java without having to use the "Constants." prefix.  Some values were already duplicated in Constants.java, so remove the redundant definitions, and move a couple of more values into Constants.

--- src/main/org/tvrenamer/controller/UpdateChecker.java
+++ src/main/org/tvrenamer/controller/UpdateChecker.java
@@ -1,16 +1,14 @@
 package org.tvrenamer.controller;
 
+import static org.tvrenamer.model.util.Constants.*;
+
 import org.tvrenamer.model.TVRenamerIOException;
-import org.tvrenamer.model.util.Constants;
-import org.tvrenamer.view.AboutDialog;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class UpdateChecker {
     private static Logger logger = Logger.getLogger(UpdateChecker.class.getName());
-
-    private static final String VERSION_URL = AboutDialog.TVRENAMER_PROJECT_URL + "/version";
 
     /**
      * Checks if a newer version is available.
@@ -21,17 +19,17 @@ public class UpdateChecker {
     public static boolean isUpdateAvailable() {
         String latestVersion;
         try {
-            latestVersion = new HttpConnectionHandler().downloadUrl(VERSION_URL);
+            latestVersion = new HttpConnectionHandler().downloadUrl(TVRENAMER_VERSION_URL);
         } catch (TVRenamerIOException e) {
             // Do nothing when an exception is thrown, just don't update display
-            logger.log(Level.SEVERE, "Exception when downloading version file " + VERSION_URL, e);
+            logger.log(Level.SEVERE, "Exception when downloading version file " + TVRENAMER_VERSION_URL, e);
             return false;
         }
 
-        boolean newVersionAvailable = latestVersion.compareToIgnoreCase(Constants.VERSION_NUMBER) > 0;
+        boolean newVersionAvailable = latestVersion.compareToIgnoreCase(VERSION_NUMBER) > 0;
 
         if (newVersionAvailable) {
-            logger.info("There is a new version available, running " + Constants.VERSION_NUMBER + ", new version is "
+            logger.info("There is a new version available, running " + VERSION_NUMBER + ", new version is "
                 + latestVersion);
             return true;
         }
