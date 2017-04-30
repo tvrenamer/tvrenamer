c761b61c178066680656dc9fe9ac377627a61ad8
John Valente <johnv02139@yahoo.com>
2017-04-29 19:32:26 -0700

    ShowNotFoundException was no longer used.

--- src/main/org/tvrenamer/model/ShowNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.tvrenamer.model;
-
-public class ShowNotFoundException extends NotFoundException {
-    private static final long serialVersionUID = 0L;
-
-    public ShowNotFoundException(String message) {
-        super(message);
-    }
-}
