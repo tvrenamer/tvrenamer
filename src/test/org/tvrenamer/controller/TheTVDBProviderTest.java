e841a5e9316c7e7a0be36a8cbe661ff71f9cd692
Episode Renamer <eprenamer@email.com>
2017-05-03 18:37:32 -0700

    Make season and episode in TVDBProviderTest be integers.
    Since we're not getting them out of a filename, there's no reason for them to be Strings, and we just end up parsing them anyway (and not as part of a test).

--- src/test/org/tvrenamer/controller/TheTVDBProviderTest.java
+++ src/test/org/tvrenamer/controller/TheTVDBProviderTest.java
@@ -123,14 +123,14 @@ public class TheTVDBProviderTest {
 
     private static class TestInput {
         public final String show;
-        public final String season;
-        public final String episode;
+        public final int seasonNum;
+        public final int episodeNum;
         public final String episodeTitle;
 
-        public TestInput(String show, String season, String episode, String episodeTitle) {
+        public TestInput(String show, int seasonNum, int episodeNum, String episodeTitle) {
             this.show = show;
-            this.season = season;
-            this.episode = episode;
+            this.seasonNum = seasonNum;
+            this.episodeNum = episodeNum;
             this.episodeTitle = episodeTitle;
         }
     }
@@ -155,73 +155,73 @@ public class TheTVDBProviderTest {
      */
     @BeforeClass
     public static void setupValues() {
-        values.add(new TestInput("game of thrones", "5", "1", "The Wars to Come"));
-        values.add(new TestInput("24", "8", "1", "Day 8: 4:00 P.M. - 5:00 P.M."));
-        values.add(new TestInput("24", "7", "18", "Day 7: 1:00 A.M. - 2:00 A.M."));
-        values.add(new TestInput("human target 2010", "1", "2", "Rewind"));
-        values.add(new TestInput("dexter", "4", "7", "Slack Tide"));
-        values.add(new TestInput("jag", "10", "1", "Hail and Farewell, Part II (2)"));
-        values.add(new TestInput("lost", "6", "5", "Lighthouse"));
-        values.add(new TestInput("warehouse 13", "1", "1", "Pilot"));
-        values.add(new TestInput("one tree hill", "7", "14", "Family Affair"));
-        values.add(new TestInput("gossip girl", "3", "15", "The Sixteen Year Old Virgin"));
-        values.add(new TestInput("smallville", "9", "14", "Conspiracy"));
-        values.add(new TestInput("smallville", "9", "15", "Escape"));
-        values.add(new TestInput("the big bang theory", "3", "18", "The Pants Alternative"));
-        values.add(new TestInput("castle 2009", "1", "9", "Little Girl Lost"));
-        values.add(new TestInput("dexter", "5", "5", "First Blood"));
-        values.add(new TestInput("lost", "2", "7", "The Other 48 Days"));
-        values.add(new TestInput("american dad", "9", "17",
+        values.add(new TestInput("game of thrones", 5, 1, "The Wars to Come"));
+        values.add(new TestInput("24", 8, 1, "Day 8: 4:00 P.M. - 5:00 P.M."));
+        values.add(new TestInput("24", 7, 18, "Day 7: 1:00 A.M. - 2:00 A.M."));
+        values.add(new TestInput("human target 2010", 1, 2, "Rewind"));
+        values.add(new TestInput("dexter", 4, 7, "Slack Tide"));
+        values.add(new TestInput("jag", 10, 1, "Hail and Farewell, Part II (2)"));
+        values.add(new TestInput("lost", 6, 5, "Lighthouse"));
+        values.add(new TestInput("warehouse 13", 1, 1, "Pilot"));
+        values.add(new TestInput("one tree hill", 7, 14, "Family Affair"));
+        values.add(new TestInput("gossip girl", 3, 15, "The Sixteen Year Old Virgin"));
+        values.add(new TestInput("smallville", 9, 14, "Conspiracy"));
+        values.add(new TestInput("smallville", 9, 15, "Escape"));
+        values.add(new TestInput("the big bang theory", 3, 18, "The Pants Alternative"));
+        values.add(new TestInput("castle 2009", 1, 9, "Little Girl Lost"));
+        values.add(new TestInput("dexter", 5, 5, "First Blood"));
+        values.add(new TestInput("lost", 2, 7, "The Other 48 Days"));
+        values.add(new TestInput("american dad", 9, 17,
                                  "The Full Cognitive Redaction of Avery Bullock by the Coward Stan Smith"));
-        values.add(new TestInput("californication", "7", "4", "Dicks"));
-        values.add(new TestInput("continuum", "3", "7", "Waning Minutes"));
-        values.add(new TestInput("elementary", "2", "23", "Art in the Blood"));
-        values.add(new TestInput("family guy", "12", "19", "Meg Stinks!"));
-        values.add(new TestInput("fargo", "1", "1", "The Crocodile's Dilemma"));
-        values.add(new TestInput("girls", "3", "11", "I Saw You"));
-        values.add(new TestInput("grimm", "3", "19", "Nobody Knows the Trubel I've Seen"));
-        values.add(new TestInput("house of cards 2013", "1", "6", "Chapter 6"));
-        values.add(new TestInput("modern family", "5", "12", "Under Pressure"));
-        values.add(new TestInput("new girl", "3", "23", "Cruise"));
-        values.add(new TestInput("nurse jackie", "6", "4", "Jungle Love"));
-        values.add(new TestInput("offspring", "5", "1", "Back in the Game"));
-        values.add(new TestInput("reign 2013", "1", "20", "Higher Ground"));
-        values.add(new TestInput("robot chicken", "7", "4", "Rebel Appliance"));
-        values.add(new TestInput("supernatural", "9", "21", "King of the Damned"));
-        values.add(new TestInput("the americans 2013", "2", "10", "Yousaf"));
-        values.add(new TestInput("the big bang theory", "7", "23", "The Gorilla Dissolution"));
-        values.add(new TestInput("the good wife", "5", "20", "The Deep Web"));
-        values.add(new TestInput("the walking dead", "4", "16", "A"));
-        values.add(new TestInput("veep", "3", "5", "Fishing"));
-        values.add(new TestInput("witches of east end", "1", "1", "Pilot"));
-        values.add(new TestInput("warehouse 13", "5", "4", "Savage Seduction"));
-        values.add(new TestInput("the 100", "2", "8", "Spacewalker"));
-        values.add(new TestInput("firefly", "1", "1", "Serenity"));
-        values.add(new TestInput("firefly", "1", "2", "The Train Job"));
-        values.add(new TestInput("firefly", "1", "3", "Bushwhacked"));
-        values.add(new TestInput("firefly", "1", "4", "Shindig"));
-        values.add(new TestInput("firefly", "1", "5", "Safe"));
-        values.add(new TestInput("firefly", "1", "6", "Our Mrs. Reynolds"));
-        values.add(new TestInput("firefly", "1", "7", "Jaynestown"));
-        values.add(new TestInput("firefly", "1", "8", "Out of Gas"));
-        values.add(new TestInput("firefly", "1", "9", "Ariel"));
-        values.add(new TestInput("firefly", "1", "10", "War Stories"));
-        values.add(new TestInput("firefly", "1", "11", "Trash"));
-        values.add(new TestInput("firefly", "1", "12", "The Message"));
-        values.add(new TestInput("firefly", "1", "13", "Heart of Gold"));
-        values.add(new TestInput("firefly", "1", "14", "Objects in Space"));
-        values.add(new TestInput("strike back", "1", "1", "Chris Ryan's Strike Back, Episode 1"));
-        values.add(new TestInput("law and order svu", "17", "05", "Community Policing"));
-        values.add(new TestInput("ncis", "13", "04", "Double Trouble"));
-        values.add(new TestInput("marvels agents of shield", "3", "3", "A Wanted (Inhu)man"));
-        values.add(new TestInput("marvels agents of shield", "3", "10", "Maveth"));
-        values.add(new TestInput("nip tuck", "6", "1", "Don Hoberman"));
-        values.add(new TestInput("the big bang theory", "10", "4", "The Cohabitation Experimentation"));
-        values.add(new TestInput("lucifer", "2", "3", "Sin-Eater"));
-        values.add(new TestInput("marvels agents of shield", "4", "3", "Uprising"));
-        values.add(new TestInput("supernatural", "11", "22", "We Happy Few"));
-        values.add(new TestInput("channel zero", "1", "1", "You Have to Go Inside"));
-        values.add(new TestInput("ncis", "14", "4", "Love Boat"));
+        values.add(new TestInput("californication", 7, 4, "Dicks"));
+        values.add(new TestInput("continuum", 3, 7, "Waning Minutes"));
+        values.add(new TestInput("elementary", 2, 23, "Art in the Blood"));
+        values.add(new TestInput("family guy", 12, 19, "Meg Stinks!"));
+        values.add(new TestInput("fargo", 1, 1, "The Crocodile's Dilemma"));
+        values.add(new TestInput("girls", 3, 11, "I Saw You"));
+        values.add(new TestInput("grimm", 3, 19, "Nobody Knows the Trubel I've Seen"));
+        values.add(new TestInput("house of cards 2013", 1, 6, "Chapter 6"));
+        values.add(new TestInput("modern family", 5, 12, "Under Pressure"));
+        values.add(new TestInput("new girl", 3, 23, "Cruise"));
+        values.add(new TestInput("nurse jackie", 6, 4, "Jungle Love"));
+        values.add(new TestInput("offspring", 5, 1, "Back in the Game"));
+        values.add(new TestInput("reign 2013", 1, 20, "Higher Ground"));
+        values.add(new TestInput("robot chicken", 7, 4, "Rebel Appliance"));
+        values.add(new TestInput("supernatural", 9, 21, "King of the Damned"));
+        values.add(new TestInput("the americans 2013", 2, 10, "Yousaf"));
+        values.add(new TestInput("the big bang theory", 7, 23, "The Gorilla Dissolution"));
+        values.add(new TestInput("the good wife", 5, 20, "The Deep Web"));
+        values.add(new TestInput("the walking dead", 4, 16, "A"));
+        values.add(new TestInput("veep", 3, 5, "Fishing"));
+        values.add(new TestInput("witches of east end", 1, 1, "Pilot"));
+        values.add(new TestInput("warehouse 13", 5, 4, "Savage Seduction"));
+        values.add(new TestInput("the 100", 2, 8, "Spacewalker"));
+        values.add(new TestInput("firefly", 1, 1, "Serenity"));
+        values.add(new TestInput("firefly", 1, 2, "The Train Job"));
+        values.add(new TestInput("firefly", 1, 3, "Bushwhacked"));
+        values.add(new TestInput("firefly", 1, 4, "Shindig"));
+        values.add(new TestInput("firefly", 1, 5, "Safe"));
+        values.add(new TestInput("firefly", 1, 6, "Our Mrs. Reynolds"));
+        values.add(new TestInput("firefly", 1, 7, "Jaynestown"));
+        values.add(new TestInput("firefly", 1, 8, "Out of Gas"));
+        values.add(new TestInput("firefly", 1, 9, "Ariel"));
+        values.add(new TestInput("firefly", 1, 10, "War Stories"));
+        values.add(new TestInput("firefly", 1, 11, "Trash"));
+        values.add(new TestInput("firefly", 1, 12, "The Message"));
+        values.add(new TestInput("firefly", 1, 13, "Heart of Gold"));
+        values.add(new TestInput("firefly", 1, 14, "Objects in Space"));
+        values.add(new TestInput("strike back", 1, 1, "Chris Ryan's Strike Back, Episode 1"));
+        values.add(new TestInput("law and order svu", 17, 05, "Community Policing"));
+        values.add(new TestInput("ncis", 13, 04, "Double Trouble"));
+        values.add(new TestInput("marvels agents of shield", 3, 3, "A Wanted (Inhu)man"));
+        values.add(new TestInput("marvels agents of shield", 3, 10, "Maveth"));
+        values.add(new TestInput("nip tuck", 6, 1, "Don Hoberman"));
+        values.add(new TestInput("the big bang theory", 10, 4, "The Cohabitation Experimentation"));
+        values.add(new TestInput("lucifer", 2, 3, "Sin-Eater"));
+        values.add(new TestInput("marvels agents of shield", 4, 3, "Uprising"));
+        values.add(new TestInput("supernatural", 11, 22, "We Happy Few"));
+        values.add(new TestInput("channel zero", 1, 1, "You Have to Go Inside"));
+        values.add(new TestInput("ncis", 14, 4, "Love Boat"));
     }
 
     // Once we have a CompletableFuture, we need to complete it.  There are a few ways, but
@@ -280,11 +280,9 @@ public class TheTVDBProviderTest {
         for (TestInput testInput : values) {
             if (testInput.episodeTitle != null) {
                 final String showName = testInput.show;
-                final String season = testInput.season;
-                final String episode = testInput.episode;
+                final int seasonNum = testInput.seasonNum;
+                final int episodeNum = testInput.episodeNum;
                 try {
-                    final int seasonNum = Integer.parseInt(season);
-                    final int episodeNum = Integer.parseInt(episode);
                     final Show show = testQueryShow(testInput, showName);
                     final CompletableFuture<String> future = new CompletableFuture<>();
                     ListingsLookup.getListings(show, new ShowListingsListener() {
@@ -309,7 +307,7 @@ public class TheTVDBProviderTest {
                     assertEquals(testInput.episodeTitle, got);
                 } catch (TimeoutException e) {
                     String failMsg = "timeout trying to query for " + showName
-                        + ", season " + season + ", episode " + episode;
+                        + ", season " + seasonNum + ", episode " + episodeNum;
                     String exceptionMessage = e.getMessage();
                     if (exceptionMessage != null) {
                         failMsg += exceptionMessage;
@@ -319,7 +317,7 @@ public class TheTVDBProviderTest {
                     fail(failMsg);
                 } catch (Exception e) {
                     String failMsg = "failure trying to query for " + showName
-                        + ", season " + season + ", episode " + episode
+                        + ", season " + seasonNum + ", episode " + episodeNum
                         + e.getClass().getName() + " ";
                     String exceptionMessage = e.getMessage();
                     if (exceptionMessage != null) {
