7e70d70699c0e057cf792bd9b849d1b853c92af1
Episode Renamer <eprenamer@email.com>
2017-05-08 14:35:14 -0700

    Improve Show.java
    Mostly adding comments.
    
    Move seasons.clear() down from the API methods, into the common inner method.  Don't bother with import static for IMDB, just qualify it with "Constants", and change method to indicate that it's IMDB.  Change "conflicts" to "problems".  Eliminate unused "toLongString" method which printed every episode.

--- src/main/org/tvrenamer/model/Show.java
+++ src/main/org/tvrenamer/model/Show.java
@@ -1,10 +1,9 @@
 package org.tvrenamer.model;
 
-import static org.tvrenamer.model.util.Constants.IMDB_BASE_URL;
-
 import org.tvrenamer.controller.ListingsLookup;
 import org.tvrenamer.controller.ShowListingsListener;
 import org.tvrenamer.controller.util.StringUtils;
+import org.tvrenamer.model.util.Constants;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -18,9 +17,37 @@ import java.util.logging.Logger;
 public class Show implements Comparable<Show> {
     private static Logger logger = Logger.getLogger(Show.class.getName());
 
+    /**
+     * Values to indicate lack of information about where an episode occurs
+     * within a show's run.  Season 0 is a valid value, as it is used for
+     * special episodes, and therefore, -1 is used as the sentinel for "not
+     * known".  For episode number, episode 0 is not used, and therefore is
+     * available for a sentinel.
+     */
     public static final int NO_SEASON = -1;
     public static final int NO_EPISODE = 0;
 
+    /**
+     * Episode numbers are not definitive.  Production companies sometimes
+     * re-order them.  In particular, they take liberties when releasing
+     * DVDs.  The TVDB tries to keep track of the original, production order,
+     * as well as the DVD ordering (when applicable).  The truth is that some
+     * shows still have ambiguity beyond these options, but those are the two
+     * basic options available.
+     *
+     * Therefore, within the code, we offer the option to order a show based
+     * on production ordering, or based on DVD ordering.  Or, we allow it to
+     * be set to "GUESS", which means that we look at all the episode information
+     * we have, and use DVD ordering if we have DVD information on enough of
+     * them, otherwise we choose production ordering.
+     *
+     * We also have an "absolute" ordering option, though this hasn't been
+     * tested well.
+     *
+     * Currently, we actually do not have anything in the UI to expose this to
+     * users.  The default value is "GUESS", and that's all that will be used,
+     * outside of the unit tests.  But we may be able to expose it in the future.
+     */
     private enum NumberingScheme {
         GUESS,
         REGULAR,
@@ -58,6 +85,9 @@ public class Show implements Comparable<Show> {
      */
     private static Map<String, Show> knownShows = new ConcurrentHashMap<>();
 
+    /*
+     * More instance variables
+     */
     private final String id;
     private final String name;
     private final String dirName;
@@ -69,6 +99,17 @@ public class Show implements Comparable<Show> {
     // Not final.  Could be changed during the program's run.
     private NumberingScheme numberingScheme = NumberingScheme.GUESS;
 
+    /**
+     * Create a Show object for a show that the provider knows about.  Initially
+     * we just get the show's name and ID, and an IMDB ID if known, but soon the
+     * Show will be augmented with all of its episodes.
+     *
+     * This class should not be used (directly) for any kind of "stand in".
+     *
+     * @param id
+     * @param name
+     * @param imdb
+     */
     protected Show(String id, String name, String imdb) {
         this.id = id;
         this.name = name;
@@ -117,22 +158,63 @@ public class Show implements Comparable<Show> {
         ListingsLookup.getListings(this, listener);
     }
 
+    /**
+     * Get this Show's ID, as a String.
+     *
+     * @return ID
+     *            the ID of the show from the provider, as a String
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Get this Show's actual, well-formatted name.  This may include a distinguisher,
+     * such as a year, if the Show's name is not unique.  This may contain punctuation
+     * characters which are not suitable for filenames, as well as non-ASCII characters.
+     *
+     * @return show name
+     *            the name of the show from the provider
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get a string representing the Show's name, appropriate for use as a directory
+     * name (or anywhere else within a filename).
+     *
+     * @return directory
+     *            the name of the show, made appropriate for use within a file path
+     */
     public String getDirName() {
         return dirName;
     }
 
-    public String getUrl() {
-        return (imdb == null) ? "" : IMDB_BASE_URL + imdb;
+    /**
+     * Get a string version of the IMDB URL, if the IMDB id is known.
+     *
+     * @return URL
+     *            the IMDB URL for this show, if known
+     */
+    public String getImdbUrl() {
+        return (imdb == null) ? "" : Constants.IMDB_BASE_URL + imdb;
     }
 
+    /**
+     * Add an episode to a season's index of episodes.
+     *
+     * This method is independent of which number scheme is being used.  That is,
+     * it's up to the caller to pass in the correct arguments for the current
+     * numbering scheme.
+     *
+     * @param seasonNum
+     *           the season of the episode to return
+     * @param episodeNum
+     *           the episode, within the given season, of the episode to return
+     * @param episode
+     *           the episode to add at the given indices
+     */
     private void addEpisodeToSeason(int seasonNum, int episodeNum, Episode episode) {
         // Check to see if there's already an existing episode.  Only applies if we
         // have a valid season number and episode number.
@@ -172,7 +254,20 @@ public class Show implements Comparable<Show> {
         }
     }
 
+    /**
+     * Build an index of this show's episodes, by season and episode number,
+     * according to the given numbering scheme.
+     *
+     * Does not change the episode list at all; just organizes them into seasons
+     * and episode numbers.
+     *
+     * Clears the season index before beginning, and iterates over all known episodes.
+     *
+     * @param effective
+     *           the numbering scheme to use
+     */
     private void indexEpisodesBySeason(NumberingScheme effective) {
+        seasons.clear();
         for (Episode episode : episodes.values()) {
             if (episode == null) {
                 logger.severe("internal error creating episodes for " + name);
@@ -208,6 +303,14 @@ public class Show implements Comparable<Show> {
         }
     }
 
+    /**
+     * Build an index of this show's episodes, by season and episode number,
+     * according to the current numbering scheme.
+     *
+     * If the current numbering scheme is "GUESS", analyzes the episodes and
+     * decides if it's suitable to use the DVD ordering, and if not, uses the
+     * standard production ordering.
+     */
     private void indexEpisodesBySeason() {
         if (numberingScheme == NumberingScheme.GUESS) {
             int withDVD = 0;
@@ -239,8 +342,19 @@ public class Show implements Comparable<Show> {
         }
     }
 
-    private void dealWithConflicts(List<EpisodeInfo> conflicts) {
-        for (EpisodeInfo info : conflicts) {
+    /**
+     * Log a message about each episode of this Show for which we found a problem.
+     * Generally a "problem" means that we have found two (or more) episodes with
+     * the same season and episode information.  Another problem could be that we
+     * got a null episodeInfo, though there's very little information we can give,
+     * in that case.
+     *
+     * @param problems
+     *    a list of the EpisodeInfos that caused problems while we were adding them
+     *    to the Show
+     */
+    private void logEpisodeProblems(List<EpisodeInfo> problems) {
+        for (EpisodeInfo info : problems) {
             if (info == null) {
                 logger.warning("received null episode info");
             } else {
@@ -255,6 +369,19 @@ public class Show implements Comparable<Show> {
         }
     }
 
+    /**
+     * Add a single episode to this Show's list.  Does not add the episode to the
+     * (season number/episode number) index, nor does it generate any log messages
+     * if anything goes wrong.  It is expected that the caller will handle any of
+     * that, if desired.
+     *
+     * @param info
+     *    information about the Episode, downloaded from the provider
+     * @return true if the episode was added successfully
+     *         false if anything went wrong, such as:
+     *         <ul><li>if the episode couldn't be parsed</li>
+     *             <li>if an  episode with the given ID was already present</li></ul>
+     */
     public boolean addOneEpisode(final EpisodeInfo info) {
         if (info != null) {
             String episodeId = info.episodeId;
@@ -268,43 +395,113 @@ public class Show implements Comparable<Show> {
         return false;
     }
 
+    /**
+     * Creates Episodes, and adds them to this Show, for each of the given EpisodeInfos.
+     * Relies on addOneEpisode() to create and verify the episode.  Collects failures
+     * from addOneEpisode(), and logs messages about them.  Generally a "problem" means
+     * that we have found two (or more) episodes with the same season and episode
+     * information.  Another problem could be that we got a null episodeInfo, though
+     * there's very little information we can give, in that case.
+     *
+     * After all the episodes are added, creates an index of the episodes by season and
+     * episode number, according to the current numbering scheme.
+     *
+     * @param infos
+     *    an array containing information about the episodes, downloaded from the provider
+     */
     public void addEpisodes(final EpisodeInfo[] infos) {
-        List<EpisodeInfo> conflicts = new LinkedList<>();
+        List<EpisodeInfo> problems = new LinkedList<>();
         for (EpisodeInfo info : infos) {
             boolean added = addOneEpisode(info);
             if (!added) {
-                conflicts.add(info);
+                problems.add(info);
             }
         }
         indexEpisodesBySeason();
-        dealWithConflicts(conflicts);
+        logEpisodeProblems(problems);
     }
 
+    /**
+     * Sets the preferred numbering scheme to be "heuristic", which means that we would
+     * prefer to use DVD ordering for the episodes, but will use standard production
+     * ordering if not enough episodes have DVD numbering information.
+     *
+     * Note that the decision to use DVD or production numbering is on a per-Show basis.
+     * If we decide to use DVD numbering, and there are a few episodes that do not have
+     * DVD information, we don't "fall back" to the other ordering.  Those episodes are
+     * simply not indexed (though they continue to exist in the show's list of episodes.)
+     *
+     * Then, actually rebuilds the index based on this preference.
+     */
     public synchronized void preferHeuristicOrdering() {
-        seasons.clear();
         numberingScheme = NumberingScheme.GUESS;
         indexEpisodesBySeason();
     }
 
+    /**
+     * Sets the preferred numbering scheme to be "production", which means that we
+     * use the standard production ordering for indexing episodes.
+     *
+     * Note that the decision to use DVD or production numbering is on a per-Show basis.
+     * In the unlikely case that there are a few episodes that do not have standard
+     * production ordering information, but do have DVD information, we don't "fall back"
+     * to DVD ordering for those episodes.  The episodes are simply not indexed (though
+     * they continue to exist in the show's list of episodes.)
+     *
+     * Then, actually rebuilds the index based on this preference.
+     */
     public synchronized void preferProductionOrdering() {
-        seasons.clear();
         numberingScheme = NumberingScheme.REGULAR;
         indexEpisodesBySeason(NumberingScheme.REGULAR);
     }
 
+    /**
+     * Sets the preferred numbering scheme to be "DVD", which means that we use the
+     * DVD ordering for indexing episodes.
+     *
+     * Note that the decision to use DVD or production numbering is on a per-Show basis.
+     * If there are a few episodes that do not have DVD information, we don't "fall
+     * back" to the production ordering.  Those episodes are simply not indexed (though
+     * they continue to exist in the show's list of episodes.)
+     *
+     * Then, actually rebuilds the index based on this preference.
+     */
     public synchronized void preferDvdOrdering() {
-        seasons.clear();
         numberingScheme = NumberingScheme.DVD_RELEASE;
         indexEpisodesBySeason(NumberingScheme.DVD_RELEASE);
     }
 
+    /**
+     * Sets the preferred numbering scheme to be "absolute", which means that we use the
+     * absolute (seasonless) numbering for indexing episodes.
+     *
+     * Then, actually rebuilds the index based on this preference.
+     */
     // Private -- not supported -- haven't tested.  TODO.
     private synchronized void preferAbsoluteOrdering() {
-        seasons.clear();
         numberingScheme = NumberingScheme.ABSOLUTE;
         indexEpisodesBySeason(NumberingScheme.ABSOLUTE);
     }
 
+    /**
+     * Look up an episode for the given season and episode of this show.
+     * Returns null if no such episode was found.
+     *
+     * Note that the value this returns is dependent on the numbering scheme
+     * in use.  There is not always a definitive answer for which episode goes
+     * in which spot.
+     *
+     * In the future, we may be able to expand it so that we use other
+     * meta-information to try to determine which episode a given filename
+     * refers to.  For now, we just go with season number and episode number.
+     *
+     * @param seasonNum
+     *           the season of the episode to return
+     * @param episodeNum
+     *           the episode, within the given season, of the episode to return
+     * @return the episode indexed at the given season and episode of this show.
+     *    Null if no such episode was found.
+     */
     public Episode getEpisode(int seasonNum, int episodeNum) {
         Map<Integer, Episode> season = seasons.get(seasonNum);
         if (season == null) {
@@ -318,23 +515,52 @@ public class Show implements Comparable<Show> {
         return episode;
     }
 
+    /**
+     * Find out whether or not there are seasons associated with this show.
+     * Generally this indicates that the show's listings have been downloaded,
+     * the episodes have been organized into seasons, and the show is ready to go.
+     *
+     * @return a count of how many seasons we have for this Show
+     */
     public boolean hasSeasons() {
+        return (seasons.size() > 0);
+    }
+
+    /**
+     * Find out whether or not there are episodes associated with this show.
+     * Generally this indicates that the show's listings have been downloaded
+     * and the show is ready to go.
+     *
+     * @return a count of how many episodes we have for this Show
+     */
+    public boolean hasEpisodes() {
         return (episodes.size() > 0);
     }
 
+    /**
+     * Get a count of how many episodes we have for this Show.
+     *
+     * @return a count of how many episodes we have for this Show
+     */
     public int getEpisodeCount() {
         return episodes.size();
     }
 
+    /**
+     * Standard object method to represent this Show as a string.
+     *
+     * @return string version of this
+     */
     @Override
     public String toString() {
         return "Show [" + name + ", id=" + id + ", imdb=" + imdb + ", " + episodes.size() + " episodes]";
     }
 
-    public String toLongString() {
-        return "Show [id=" + id + ", name=" + name + ", imdb=" + imdb + ", episodes =" + episodes + "]";
-    }
-
+    /**
+     * Standard compareTo method to make this class comparable
+     *
+     * @return this object's relative ordering compared to the given object
+     */
     @Override
     public int compareTo(Show other) {
         return Integer.parseInt(other.id) - Integer.parseInt(this.id);
