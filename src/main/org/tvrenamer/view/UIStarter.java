fb96542a5faa74f84be8d529eba20b765534912b
Episode Renamer <eprenamer@email.com>
2017-05-09 10:04:19 -0700

    Make two classes final.
    Generally it's considered good practice that if a class is not designed to be extended, make it final.  But moreover in the case of these two classes, which use GetResource, findbugs warns that it may be unsafe if class is extended.  So make sure these classes cannot be extended.

--- src/main/org/tvrenamer/view/UIStarter.java
+++ src/main/org/tvrenamer/view/UIStarter.java
@@ -73,7 +73,7 @@ import java.util.Queue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class UIStarter implements Observer,  AddEpisodeListener {
+public final class UIStarter implements Observer,  AddEpisodeListener {
     private static Logger logger = Logger.getLogger(UIStarter.class.getName());
     private static final int SELECTED_COLUMN = 0;
     private static final int CURRENT_FILE_COLUMN = 1;
