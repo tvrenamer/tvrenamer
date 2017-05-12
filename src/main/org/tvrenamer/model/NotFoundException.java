7667d14044d2ba36c8c2e789b35044ee9010184b
Episode Renamer <eprenamer@email.com>
2017-05-11 23:20:22 -0700

    NotFoundException was no longer being thrown.
    It was still being caught, though.  Remove the class, and remove the code that caught it and that handled it.  Rearrange the code a bit, in the method that had been catching it.

--- src/main/org/tvrenamer/model/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.tvrenamer.model;
-
-public class NotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 0L;
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
