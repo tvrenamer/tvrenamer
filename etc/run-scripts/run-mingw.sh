f2cf5af5ed2fd0492279647243e0ad71434b4cbf
Episode Renamer <eprenamer@email.com>
2017-05-12 14:25:22 -0700

    Make previous version easier to deal with
    For comparison purposes, it is sometimes useful to be able to go back and see what the previous version did.  It can be painful to deal with, but obviously we don't want to change anything of consequence -- that would defeat the purpose of going back to the previous version.  However, I think updating .gitignore and adding a run script does not muddle anything.

---
+++ etc/run-scripts/run-mingw.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+# run-mingw.sh -- quick and dirty (albeit heavily-commented) script to run
+#  the TVRenamer app without "launch4j" or any similar functionality.
+
+# This will optionally run "ant compile", which builds a bunch of class
+# files in the "out" directory.  (If you don't enable that option, we
+# assume it's because you've already run ant.)
+
+# I wrote this to be run from "git bash" for Windows, which is mingw32.
+# It has specific functionality to use Windows paths, and to choose the
+# right SWT jarfile.  To run on a different platform, the script would
+# need to be edited slightly.  To try to make sure you do that, it actually
+# doesn't run on any other platforms.  But again, trivial to change that.
+
+# This script automatically redirects stdout into a hard-coded location.
+# It would be very easy to edit the script if you don't like that.
+
+# If anyone wants to run anywhere else, make it work
+if [ "$MSYSTEM" != "MINGW32" ]
+then
+  echo "Script only tested on MinGW 6.1 and 6.2.  Edit it for your platform."
+  exit 1
+fi
+
+# local libraries -- hard-coded.  The first one is platform-specific.
+loclibs="swt-win64-4.3.jar jedit-4.3.2-IOUtilities.jar commons-codec-1.4.jar xstream-1.3.1.jar xpp3_min-1.1.4.jar"
+
+# Other Windows Bourne shells, like Cygwin, provide specific functionality
+# for going between Unix-style and Windows-style paths.  I don't find any
+# for mingw32.  Note we don't need to change slashes.  Java is ok with
+# forward slashes on Windows.  But it doesn't understand mingw's virtual
+# drives, so we need a regular drive letter.
+windowsize ()
+{
+  echo $1 | sed 's,^/\([a-zA-Z]\)/,\1:/,'
+}
+
+# To go in the other direction, we do need to change the slashes.
+unixize ()
+{
+  echo $1 | sed 's,^\([a-zA-Z]\):,/\1,' | tr '\\' '/'
+}
+
+usage ()
+{
+  echo "Error: unrecognized argument $1"
+  echo "$0 [ -build ]"
+  exit 3
+}
+
+# The script should be runnable from anywhere.  Stash the current location
+# to come back to it.
+startdir=`pwd`
+
+userhome=${HOME}
+if [ -n "${USERPROFILE}" ]
+then
+  userhome=`unixize ${USERPROFILE}`
+fi
+
+if [ -d ${userhome}/.tvrenamer ]
+then
+    /bin/mv ${userhome}/.tvrenamer/prefs.xml ${userhome}/prefs.xml
+    /bin/rm -rf ${userhome}/.tvrenamer
+    /bin/mv ${userhome}/prefs.xml ${userhome}/.tvrenamer
+elif [ ! -f ${userhome}/.tvrenamer ]
+then
+  echo "Error: no ${userhome}/.tvrenamer file found"
+fi
+
+# Figure out where the script lives.  That tells us the project's root directory.
+proj=`dirname $0`
+cd ${proj}/../..
+pdir=`pwd`
+proj=`windowsize $pdir`
+
+# In later versions, build.xml does this
+/bin/cp ./src/main/tvrenamer.version out/tvrenamer.version
+/bin/cp ./etc/logging.properties out/logging.properties
+
+if [ -n "$1" ]
+then
+  if [ "$1" = "-build" ]
+  then
+    shift
+    if [ -n "$1" ]
+    then
+      usage $1
+    fi
+    ant compile || exit 2
+  else
+    usage $1
+  fi
+fi
+
+# Return to where we started
+cd $startdir
+
+# Library files are checked in here
+loclibdir=${pdir}/lib/main
+
+CLASSPATH=${proj}/out
+for lib in ${loclibs}
+do
+  CLASSPATH=${CLASSPATH}';'`windowsize ${loclibdir}/${lib}`
+done
+export CLASSPATH
+
+echo $CLASSPATH
+java com.google.code.tvrenamer.view.UIStarter
