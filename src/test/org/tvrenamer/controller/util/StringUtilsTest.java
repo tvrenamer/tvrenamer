b647e5c2a93eec36b8b119ab36f8af1d5977166f
Episode Renamer <eprenamer@email.com>
2017-05-13 17:01:17 -0700

    Keep lines reasonable length
    Put the argument on the next line to avoid ridiculously long lines.

--- src/test/org/tvrenamer/controller/util/StringUtilsTest.java
+++ src/test/org/tvrenamer/controller/util/StringUtilsTest.java
@@ -92,7 +92,8 @@ public class StringUtilsTest {
     @Test
     public void testGetExtension() {
         assertEquals(".mkv", StringUtils.getExtension("dexter.407.720p.hdtv.x264-sys.mkv"));
-        assertEquals(".avi", StringUtils.getExtension("Marvels.Agents.of.S.H.I.E.L.D.S04E03.1080p.HDTV.x264-KILLERS[ettv].avi"));
+        String shield = "Marvels.Agents.of.S.H.I.E.L.D.S04E03.1080p.HDTV.x264-KILLERS[ettv].avi";
+        assertEquals(".avi", StringUtils.getExtension(shield));
         assertEquals(".mp4", StringUtils.getExtension("/TV/Dexter/S05E05 First Blood.mp4"));
         assertEquals("", StringUtils.getExtension("Supernatural"));
     }
@@ -104,22 +105,19 @@ public class StringUtilsTest {
                      StringUtils.makeDotTitle("'Twas the Nightmare Before Christmas"));
         assertEquals("1%", StringUtils.makeDotTitle("1%"));
         assertEquals("200(1)", StringUtils.makeDotTitle("200 (1)"));
-
-        assertEquals("Helen.Keller!The.Musical", StringUtils.makeDotTitle("Helen Keller! The Musical"));
-
-        assertEquals("And.in.Case.I.Don't.See.Ya", StringUtils.makeDotTitle("And in Case I Don't See Ya"));
-
-        assertEquals("Are.You.There.God.It's.Me,Jesus", StringUtils.makeDotTitle("Are You There God It's Me, Jesus"));
-
+        assertEquals("Helen.Keller!The.Musical",
+                     StringUtils.makeDotTitle("Helen Keller! The Musical"));
+        assertEquals("And.in.Case.I.Don't.See.Ya",
+                     StringUtils.makeDotTitle("And in Case I Don't See Ya"));
+        assertEquals("Are.You.There.God.It's.Me,Jesus",
+                     StringUtils.makeDotTitle("Are You There God It's Me, Jesus"));
         assertEquals("The.Return.of.Dorothy's.Ex(a.k.a.Stan's.Return)",
                      StringUtils.makeDotTitle("The Return of Dorothy's Ex (a.k.a. Stan's Return)"));
         assertEquals("Girls.Just.Wanna.Have.Fun...Before.They.Die",
                      StringUtils.makeDotTitle("Girls Just Wanna Have Fun...Before They Die"));
         assertEquals("Terrance&Phillip.in'Not.Without.My.Anus'",
                      StringUtils.makeDotTitle("Terrance & Phillip in 'Not Without My Anus'"));
-
         assertEquals("B&B's.B'n.B", StringUtils.makeDotTitle("B & B's B'n B"));
-
         assertEquals("AWESOM-O", StringUtils.makeDotTitle("AWESOM-O"));
         assertEquals("Coon.2-Hindsight(1)", StringUtils.makeDotTitle("Coon 2 - Hindsight (1)"));
         assertEquals("Class.Pre-Union", StringUtils.makeDotTitle("Class Pre-Union"));
@@ -139,24 +137,31 @@ public class StringUtilsTest {
                      StringUtils.makeDotTitle("Slapsgiving 2 - Revenge of the Slap"));
         assertEquals("Valentine's.Day.4-Twisted.Sister",
                      StringUtils.makeDotTitle("Valentine's Day 4 - Twisted Sister"));
-
-        assertEquals("Ro\\$e.Love\\$Mile\\$", StringUtils.makeDotTitle("Ro\\$e Love\\$ Mile\\$"));
-
+        assertEquals("Ro\\$e.Love\\$Mile\\$",
+                     StringUtils.makeDotTitle("Ro\\$e Love\\$ Mile\\$"));
         assertEquals("Believe.it.or.Not,Joe's.Walking.on.Air",
                      StringUtils.makeDotTitle("Believe it or Not, Joe's Walking on Air"));
         assertEquals("Eek,A.Penis!", StringUtils.makeDotTitle("Eek, A Penis!"));
-        assertEquals("I.Love.You,Donna.Karan(1)", StringUtils.makeDotTitle("I Love You, Donna Karan (1)"));
+        assertEquals("I.Love.You,Donna.Karan(1)",
+                     StringUtils.makeDotTitle("I Love You, Donna Karan (1)"));
     }
 
     @Test
     public void testReplacePunctuation() {
-        assertEquals("Marvels Agents of SHIELD", StringUtils.replacePunctuation("Marvel's Agents of S.H.I.E.L.D."));
-        assertEquals("Star Trek The Next Generation", StringUtils.replacePunctuation("Star Trek: The Next Generation"));
-        assertEquals("Monty Pythons Flying Circus", StringUtils.replacePunctuation("Monty Python's Flying Circus"));
-        assertEquals("Married with Children", StringUtils.replacePunctuation("Married... with Children"));
-        assertEquals("God The Devil and Bob", StringUtils.replacePunctuation("God, The Devil and Bob"));
-        assertEquals("Whats Happening", StringUtils.replacePunctuation("What's Happening!!"));
-        assertEquals("Brooklyn Nine Nine", StringUtils.replacePunctuation("Brooklyn Nine-Nine"));
+        assertEquals("Marvels Agents of SHIELD",
+                     StringUtils.replacePunctuation("Marvel's Agents of S.H.I.E.L.D."));
+        assertEquals("Star Trek The Next Generation",
+                     StringUtils.replacePunctuation("Star Trek: The Next Generation"));
+        assertEquals("Monty Pythons Flying Circus",
+                     StringUtils.replacePunctuation("Monty Python's Flying Circus"));
+        assertEquals("Married with Children",
+                     StringUtils.replacePunctuation("Married... with Children"));
+        assertEquals("God The Devil and Bob",
+                     StringUtils.replacePunctuation("God, The Devil and Bob"));
+        assertEquals("Whats Happening",
+                     StringUtils.replacePunctuation("What's Happening!!"));
+        assertEquals("Brooklyn Nine Nine",
+                     StringUtils.replacePunctuation("Brooklyn Nine-Nine"));
         assertEquals("Murder She Wrote", StringUtils.replacePunctuation("Murder, She Wrote"));
         assertEquals("Andy Barker PI", StringUtils.replacePunctuation("Andy Barker, P.I."));
         assertEquals("Sit Down Shut Up", StringUtils.replacePunctuation("Sit Down, Shut Up"));
