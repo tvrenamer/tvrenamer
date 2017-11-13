030e2ad30b26614335fa20f7b8a9f874d682b97e
Episode Renamer <eprenamer@email.com>
2017-11-12 21:04:38 -0500

    Don't rely on Show subclasses
    We have a few different flavors of "Show".  It could be that we found the show in the provider's db, or that we have created a stand-in show, or that we failed to find the show.  These are implemented using subclasses, and then users check "instanceof" to see what it is.  For now, we're keeping the subclasses, but relegating it to an implementation detail by not relying on it directly, outside of the Show class.
    
    In TheTVDBSwaggerProviderTest, add a test that the Show should not be a "failed show".  Previously we didn't bother with this, because we knew that a failed show was a subclass of a "local show", and we already verified that it wasn't a local show.  Since we're trying to be less reliant on clients knowing implementation details, add the extra (even though currently redundant) check.
    
    Now can make LocalShow package-private.

--- src/main/org/tvrenamer/model/ShowStore.java
+++ src/main/org/tvrenamer/model/ShowStore.java
@@ -198,7 +198,7 @@ public class ShowStore {
             // directly.  If, in the future, we expand ShowInformationListener so
             // that there is more information to be sent later, we'd want to edit
             // this to add the listener.
-            if (show instanceof LocalShow) {
+            if (show.isLocalShow()) {
                 listener.downloadFailed(show);
             } else {
                 listener.downloaded(show);
@@ -237,7 +237,7 @@ public class ShowStore {
             }
 
             logger.fine("Show options for '" + thisShow.getName() + "' downloaded");
-            if (thisShow instanceof FailedShow) {
+            if (thisShow.isFailedShow()) {
                 showName.nameNotFound(thisShow);
             } else {
                 showName.nameResolved(thisShow);
