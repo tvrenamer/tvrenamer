3832b0b7c264086b34a1b1e880dfaad846fb887a
Episode Renamer <eprenamer@email.com>
2017-11-14 19:37:17 -0500

    Add class for EpisodePlacement
    An episode "placement" is just its season number and episode number; i.e., the "place" it belongs in the episode index.  It is just a way to encapsulate the two pieces of information.
    
    Use EpisodePlacement in a few places instead of individual season and episode numbers.

---
+++ src/main/org/tvrenamer/model/EpisodePlacement.java
@@ -0,0 +1,11 @@
+package org.tvrenamer.model;
+
+public class EpisodePlacement {
+    public final int season;
+    public final int episode;
+
+    public EpisodePlacement(int season, int episode) {
+        this.season = season;
+        this.episode = episode;
+    }
+}
