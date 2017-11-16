b0bc028dd8470e77fbff52a2361261f0e954dd0b
Episode Renamer <eprenamer@email.com>
2017-11-15 20:38:59 -0500

    Add static method readImageFromPath to UIUtils.

--- src/main/org/tvrenamer/view/FileMoveIcon.java
+++ src/main/org/tvrenamer/view/FileMoveIcon.java
@@ -1,11 +1,8 @@
 package org.tvrenamer.view;
 
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Display;
 
 import org.tvrenamer.model.util.Constants;
-
-import java.io.InputStream;
 
 public enum FileMoveIcon {
     ADDED("/icons/SweetieLegacy/16-circle-blue.png"),
@@ -17,12 +14,6 @@ public enum FileMoveIcon {
     public final Image icon;
 
     FileMoveIcon(String path) {
-        InputStream stream = getClass().getResourceAsStream(path);
-        if (stream != null) {
-            icon = new Image(Display.getCurrent(), stream);
-        } else {
-            icon = new Image(Display.getCurrent(), Constants.ICON_PARENT_DIRECTORY
-                             + "/" + path);
-        }
+        icon = UIUtils.readImageFromPath(path, Constants.ICON_PARENT_DIRECTORY + "/" + path);
     }
 }
