13a753f1bbadf9e73ab86740a2527cc2bb17f43c
Episode Renamer <eprenamer@email.com>
2017-11-19 20:05:18 -0500

    Rework UpdateChecker
    When we want to download the latest version available, create the thread in UpdateChecker, rather than in UIStarter.
    
    Add a new type of listener, org/tvrenamer/controller/UpdateListener.java, and use it to allow the UpdateChecker to initiate the action when we know whether or not a new version is available.
    
    This helps keep a greater separation between view and controller.
    
    Move "updatesAvailable" stuff into its own method in UIStarter.
    
    Add a text string to Constants and use it instead of inlined text.

--- src/main/org/tvrenamer/controller/UpdateChecker.java
+++ src/main/org/tvrenamer/controller/UpdateChecker.java
@@ -3,6 +3,7 @@ package org.tvrenamer.controller;
 import static org.tvrenamer.model.util.Constants.*;
 
 import org.tvrenamer.model.TVRenamerIOException;
+import org.tvrenamer.model.UserPreferences;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,29 +11,62 @@ import java.util.logging.Logger;
 public class UpdateChecker {
     private static final Logger logger = Logger.getLogger(UpdateChecker.class.getName());
 
+    private static Boolean newVersionAvailable = null;
+    private static String latestVersion;
+
     /**
      * Checks if a newer version is available.
      *
      * @return true if a new version is available, false if there is no new version or
      *          if an error has occurred
      */
-    public static boolean isUpdateAvailable() {
-        String latestVersion;
+    private static synchronized boolean checkIfUpdateAvailable() {
+        boolean available = false;
         try {
             latestVersion = new HttpConnectionHandler().downloadUrl(TVRENAMER_VERSION_URL);
+            available = latestVersion.compareToIgnoreCase(VERSION_NUMBER) > 0;
         } catch (TVRenamerIOException e) {
             // Do nothing when an exception is thrown, just don't update display
-            logger.log(Level.SEVERE, "Exception when downloading version file " + TVRENAMER_VERSION_URL, e);
-            return false;
+            logger.log(Level.SEVERE, "Exception when downloading version file " + TVRENAMER_VERSION_URL,
+                       e);
         }
 
-        boolean newVersionAvailable = latestVersion.compareToIgnoreCase(VERSION_NUMBER) > 0;
+        return available;
+    }
 
+    /**
+     * Checks if a newer version is available.
+     *
+     * @return true if a new version is available, false if there is no new version or
+     *          if an error has occurred
+     */
+    private static synchronized boolean isUpdateAvailable() {
+        if (newVersionAvailable == null) {
+            newVersionAvailable = checkIfUpdateAvailable();
+        }
         if (newVersionAvailable) {
-            logger.info("There is a new version available, running " + VERSION_NUMBER + ", new version is "
-                + latestVersion);
+            logger.info("There is a new version available, running " + VERSION_NUMBER
+                        + ", new version is " + latestVersion);
             return true;
         }
+        logger.finer("You have the latest version!");
         return false;
+    }
+
+    /**
+     * Notifies the listener whether or not a new version is available
+     *
+     * @param listener
+     *   the listener to update of whether or not an update is available
+     */
+    public static void notifyOfUpdate(final UpdateListener listener) {
+        Thread updateCheckThread = new Thread(() -> {
+            boolean doNotify = false;
+            if (UserPreferences.getInstance().checkForUpdates()) {
+                doNotify = isUpdateAvailable();
+            }
+            listener.notifyUpdateStatus(doNotify);
+        });
+        updateCheckThread.start();
     }
 }
