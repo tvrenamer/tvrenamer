4b96b6b503996cd9daa026084069c59afdfddc86
Episode Renamer <eprenamer@email.com>
2018-01-20 02:47:40 -0500

    Move column header texts into Constants

--- src/main/org/tvrenamer/view/ResultsTable.java
+++ src/main/org/tvrenamer/view/ResultsTable.java
@@ -538,7 +538,7 @@ public final class ResultsTable implements Observer, AddEpisodeListener {
 
     private void setupColumns() {
         final TableColumn selectedColumn = new TableColumn(swtTable, SWT.LEFT);
-        selectedColumn.setText("Selected");
+        selectedColumn.setText(CHECKBOX_HEADER);
         selectedColumn.setWidth(60);
         selectedColumn.addSelectionListener(new SelectionAdapter() {
             @Override
@@ -548,7 +548,7 @@ public final class ResultsTable implements Observer, AddEpisodeListener {
         });
 
         final TableColumn sourceColumn = new TableColumn(swtTable, SWT.LEFT);
-        sourceColumn.setText("Current File");
+        sourceColumn.setText(SOURCE_HEADER);
         sourceColumn.setWidth(550);
         sourceColumn.addSelectionListener(new SelectionAdapter() {
             @Override
@@ -568,7 +568,7 @@ public final class ResultsTable implements Observer, AddEpisodeListener {
         });
 
         final TableColumn statusColumn = new TableColumn(swtTable, SWT.LEFT);
-        statusColumn.setText("Status");
+        statusColumn.setText(STATUS_HEADER);
         statusColumn.setWidth(60);
         statusColumn.addSelectionListener(new SelectionAdapter() {
             @Override
