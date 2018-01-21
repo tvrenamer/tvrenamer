663e33f91e413d443374ee9ddc381dcb8d5e6ab4
Episode Renamer <eprenamer@email.com>
2018-01-21 17:08:46 -0500

    Add EpisodePlacement toString method

--- src/main/org/tvrenamer/model/EpisodePlacement.java
+++ src/main/org/tvrenamer/model/EpisodePlacement.java
@@ -1,5 +1,7 @@
 package org.tvrenamer.model;
 
+import org.tvrenamer.controller.util.StringUtils;
+
 public class EpisodePlacement {
     public final int season;
     public final int episode;
@@ -7,5 +9,11 @@ public class EpisodePlacement {
     public EpisodePlacement(int season, int episode) {
         this.season = season;
         this.episode = episode;
+    }
+
+    @Override
+    public String toString() {
+        return "S" + StringUtils.zeroPadTwoDigits(season)
+            + "E" + StringUtils.zeroPadTwoDigits(episode);
     }
 }
