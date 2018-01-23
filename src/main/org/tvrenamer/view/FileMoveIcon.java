0837d949fbabada701040be7028ffd5296c6724f
Episode Renamer <eprenamer@email.com>
2018-01-22 21:40:34 -0500

    Rework FileMoveIcon to be a class
    It was formerly an enum.  Make its former contents be an enum within the class (called "Status")
    
    The main thing this is addressing is, if you have a TableItem, you cannot tell what state it's in.  The only thing you can get is the Image object, which doesn't tell you anything.  By making it a class, we introduce private hash tables that map from the Image object back to the state.
    
    One reason we might want to know what state a TableItem is in is because we want to sort by that column.  Previously, we just sorted by whatever "toString()" returned for the Image object.  That caused items which had the same status to be grouped together, at least.  But there was no logic to the ordering.
    
    Now we introduce a specified ordering, and use that for sorting.

--- src/main/org/tvrenamer/view/FileMoveIcon.java
+++ src/main/org/tvrenamer/view/FileMoveIcon.java
@@ -1,20 +1,66 @@
 package org.tvrenamer.view;
 
+import static org.tvrenamer.model.util.Constants.*;
+
 import org.eclipse.swt.graphics.Image;
 
-import org.tvrenamer.model.util.Constants;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public enum FileMoveIcon {
-    ADDED("/icons/SweetieLegacy/16-circle-blue.png"),
-    DOWNLOADING("/icons/SweetieLegacy/16-clock.png"),
-    OPTIONS("/icons/SweetieLegacy/16-comment-question.png"),
-    RENAMING(Constants.RENAMING_PATH),
-    SUCCESS("/icons/SweetieLegacy/16-em-check.png"),
-    FAIL("/icons/SweetieLegacy/16-em-cross.png");
+public class FileMoveIcon {
+    public enum Status {
+        SUCCESS,
+        OPTIONS,
+        ADDED,
+        DOWNLOADING,
+        RENAMING,
+        FAIL;
+    }
 
-    public final Image icon;
+    private static final String ICON_PATH = "/icons/SweetieLegacy/";
 
-    FileMoveIcon(String path) {
-        icon = UIUtils.readImageFromPath(path, Constants.ICON_PARENT_DIRECTORY + "/" + path);
+    private final Status status;
+    private final Image image;
+    private final String ordering;
+
+    FileMoveIcon(String ordering, Status status, String imageFilename) {
+        this.ordering = ordering;
+        this.status = status;
+        this.image = UIUtils.readImageFromPath(ICON_PATH + imageFilename);
+    }
+
+    private static final FileMoveIcon[] STANDARD_STATUSES = {
+        new FileMoveIcon("a", Status.SUCCESS, "16-em-check.png"),
+        new FileMoveIcon("b", Status.OPTIONS, "16-comment-question.png"),
+        new FileMoveIcon("c", Status.ADDED, "16-circle-blue.png"),
+        new FileMoveIcon("d", Status.DOWNLOADING, "16-clock.png"),
+        new FileMoveIcon("e", Status.RENAMING, "16-em-pencil.png"),
+        new FileMoveIcon("f", Status.FAIL, "16-em-cross.png")
+    };
+
+    private static final Map<Status, FileMoveIcon> MAPPING = new ConcurrentHashMap<>();
+    private static final Map<Image, FileMoveIcon> IMAGES = new ConcurrentHashMap<>();
+
+    static {
+        for (FileMoveIcon state : STANDARD_STATUSES) {
+            MAPPING.put(state.status, state);
+            IMAGES.put(state.image, state);
+        }
+    }
+
+    public static Image getIcon(final Status status) {
+        FileMoveIcon state = MAPPING.get(status);
+        if (state == null) {
+            return null;
+        }
+        return state.image;
+    }
+
+    public static String getImagePriority(final Image img) {
+        FileMoveIcon state = IMAGES.get(img);
+        if (state == null) {
+            return null;
+        }
+        return state.ordering;
     }
 }
