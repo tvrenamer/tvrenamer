84cda8e9063d0e3f0f27503cf6901f6154bc1dce
Episode Renamer <eprenamer@email.com>
2018-06-07 13:03:14 -0700

    Revert "Define overlay image in Constants"
    This reverts commit f795ff1e21261cb37b4de03f56c6e2dd5df56f25.
    
    There was really no good reason to handle it outside of FileMoveIcon.

--- src/main/org/tvrenamer/view/ProgressBarUpdater.java
+++ src/main/org/tvrenamer/view/ProgressBarUpdater.java
@@ -1,17 +1,13 @@
 package org.tvrenamer.view;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.TaskItem;
 
 import org.tvrenamer.model.ProgressUpdater;
-import org.tvrenamer.model.util.Constants;
 
 public class ProgressBarUpdater implements ProgressUpdater {
-
-    private static Image OVERLAY_IMG = UIUtils.readImageFromPath(Constants.RENAMING_PATH);
 
     private final ResultsTable ui;
     private final Display display;
@@ -28,7 +24,7 @@ public class ProgressBarUpdater implements ProgressUpdater {
 
         if (taskItem != null) {
             taskItem.setProgressState(SWT.NORMAL);
-            taskItem.setOverlayImage(OVERLAY_IMG);
+            taskItem.setOverlayImage(FileMoveIcon.getIcon(FileMoveIcon.Status.RENAMING));
         }
     }
 
