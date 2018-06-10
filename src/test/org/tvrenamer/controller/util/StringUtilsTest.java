89f9886b9f6481511598dff4062df58ce1618dd1
Episode Renamer <eprenamer@email.com>
2018-06-10 01:43:36 -0700

    Fix ordering of arguments in StringUtilsTest
    assertEquals takes the expected value as the first argument, and the actual value as the second.  By reversing them, when there was a failure, we'd get a confusing error message.  Swap the arguments so that we'd get the right error message.

--- src/test/org/tvrenamer/controller/util/StringUtilsTest.java
+++ src/test/org/tvrenamer/controller/util/StringUtilsTest.java
@@ -8,52 +8,52 @@ public class StringUtilsTest {
 
     @Test
     public void testSanitiseTitleBackslash() {
-        assertEquals(StringUtils.sanitiseTitle("Test\\"), "Test-");
+        assertEquals("Test-", StringUtils.sanitiseTitle("Test\\"));
     }
 
     @Test
     public void testSanitiseTitleForwardSlash() {
-        assertEquals(StringUtils.sanitiseTitle("Test/"), "Test-");
+        assertEquals("Test-", StringUtils.sanitiseTitle("Test/"));
     }
 
     @Test
     public void testSanitiseTitleColon() {
-        assertEquals(StringUtils.sanitiseTitle("Test:"), "Test -");
+        assertEquals("Test -", StringUtils.sanitiseTitle("Test:"));
     }
 
     @Test
     public void testSanitiseTitlePipe() {
-        assertEquals(StringUtils.sanitiseTitle("Test|"), "Test-");
+        assertEquals("Test-", StringUtils.sanitiseTitle("Test|"));
     }
 
     @Test
     public void testSanitiseTitleAsterisk() {
-        assertEquals(StringUtils.sanitiseTitle("Test*"), "Test-");
+        assertEquals("Test-", StringUtils.sanitiseTitle("Test*"));
     }
 
     @Test
     public void testSanitiseTitleQuestionMark() {
-        assertEquals(StringUtils.sanitiseTitle("Test?"), "Test");
+        assertEquals("Test", StringUtils.sanitiseTitle("Test?"));
     }
 
     @Test
     public void testSanitiseTitleGreaterThan() {
-        assertEquals(StringUtils.sanitiseTitle("Test>"), "Test");
+        assertEquals("Test", StringUtils.sanitiseTitle("Test>"));
     }
 
     @Test
     public void testSanitiseTitleLessThan() {
-        assertEquals(StringUtils.sanitiseTitle("Test<"), "Test");
+        assertEquals("Test", StringUtils.sanitiseTitle("Test<"));
     }
 
     @Test
     public void testSanitiseTitleDoubleQuote() {
-        assertEquals(StringUtils.sanitiseTitle("Test\""), "Test'");
+        assertEquals("Test'", StringUtils.sanitiseTitle("Test\""));
     }
 
     @Test
     public void testSanitiseTitleTilde() {
-        assertEquals(StringUtils.sanitiseTitle("Test`"), "Test'");
+        assertEquals("Test'", StringUtils.sanitiseTitle("Test`"));
     }
 
     @Test
