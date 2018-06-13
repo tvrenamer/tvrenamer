4079094e04ca035be0ff088161ccc6d6ec447895
John Valente <johnv02139@yahoo.com>
2018-06-12 17:32:10 -0700

    Move showMessageBox into UIStarter
    Change the inner class in AboutDialog to nonstatic so that it can access the ui instance variable.  Move a number of imports from UIUtils to UIStarter, as well.
    
    Now we no longer need to pass a reference to the Shell to UIUtils.

--- src/main/org/tvrenamer/view/UIStarter.java
+++ src/main/org/tvrenamer/view/UIStarter.java
@@ -1,12 +1,12 @@
 package org.tvrenamer.view;
 
 import static org.tvrenamer.model.util.Constants.*;
-import static org.tvrenamer.view.UIUtils.showMessageBox;
 
 import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.Shell;
 
@@ -14,10 +14,13 @@ import org.tvrenamer.model.SWTMessageBoxType;
 import org.tvrenamer.model.UserPreference;
 import org.tvrenamer.model.UserPreferences;
 
+import java.awt.HeadlessException;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
 
 public final class UIStarter implements Observer {
     private static final Logger logger = Logger.getLogger(UIStarter.class.getName());
@@ -43,6 +46,47 @@ public final class UIStarter implements Observer {
         return defaultFont;
     }
 
+    public void showMessageBox(final SWTMessageBoxType type, final String title,
+                               final String message, final Exception exception)
+    {
+        if (shell == null) {
+            // Shell not established yet, try using JOptionPane instead
+            try {
+                JOptionPane.showMessageDialog(null, message);
+                return;
+            } catch (HeadlessException he) {
+                logger.warning("Could not show message graphically: " + message);
+                return;
+            }
+        }
+
+        display.syncExec(() -> {
+            MessageBox msgBox = new MessageBox(shell, type.getSwtIconValue());
+            msgBox.setText(title);
+
+            if (exception == null) {
+                msgBox.setMessage(message);
+            } else {
+                msgBox.setMessage(message + "\n" + exception.getLocalizedMessage());
+            }
+
+            msgBox.open();
+        });
+    }
+
+    /**
+     * Show a message box of the given type with the given message content and window title.
+     *
+     * @param type the {@link SWTMessageBoxType} to create
+     * @param title the window title
+     * @param message the message content
+     */
+    public void showMessageBox(final SWTMessageBoxType type,
+                               final String title, final String message)
+    {
+        showMessageBox(type, title, message, null);
+    }
+
     private void positionWindow() {
         // place the window near the lower right-hand corner
         Monitor primary = display.getPrimaryMonitor();
@@ -63,9 +107,6 @@ public final class UIStarter implements Observer {
 
         GridLayout shellGridLayout = new GridLayout(3, false);
         shell.setLayout(shellGridLayout);
-
-        // Setup the util class
-        UIUtils.setShell(shell);
 
         // Create the main window
         resultsTable = new ResultsTable(this);
--- src/main/org/tvrenamer/view/UIUtils.java
+++ src/main/org/tvrenamer/view/UIUtils.java
@@ -4,76 +4,16 @@ import static org.tvrenamer.model.util.Constants.*;
 
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Shell;
 
-import org.tvrenamer.model.SWTMessageBoxType;
-
-import java.awt.HeadlessException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
 
-import javax.swing.JOptionPane;
-
 class UIUtils {
     private static final Logger logger = Logger.getLogger(UIUtils.class.getName());
 
-    private static Shell shell = null;
-
     private UIUtils() {
         // utility class; prevent instantiation
-    }
-
-    /**
-     * Give this class a pointer to the UI's shell.
-     *
-     * @param shell
-     *            the shell to use.
-     */
-    public static void setShell(Shell shell) {
-        UIUtils.shell = shell;
-    }
-
-    public static void showMessageBox(final SWTMessageBoxType type, final String title,
-                                      final String message, final Exception exception)
-    {
-        if (shell == null) {
-            // Shell not established yet, try using JOptionPane instead
-            try {
-                JOptionPane.showMessageDialog(null, message);
-                return;
-            } catch (HeadlessException he) {
-                logger.warning("Could not show message graphically: " + message);
-                return;
-            }
-        }
-
-        Display.getDefault().syncExec(() -> {
-            MessageBox msgBox = new MessageBox(shell, type.getSwtIconValue());
-            msgBox.setText(title);
-
-            if (exception == null) {
-                msgBox.setMessage(message);
-            } else {
-                msgBox.setMessage(message + "\n" + exception.getLocalizedMessage());
-            }
-
-            msgBox.open();
-        });
-    }
-
-    /**
-     * Show a message box of the given type with the given message content and window title.
-     *
-     * @param type the {@link SWTMessageBoxType} to create
-     * @param title the window title
-     * @param message the message content
-     */
-    public static void showMessageBox(final SWTMessageBoxType type,
-                                      final String title, final String message)
-    {
-        showMessageBox(type, title, message, null);
     }
 
     /**
