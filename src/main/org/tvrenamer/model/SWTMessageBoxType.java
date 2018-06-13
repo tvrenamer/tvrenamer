41296ae87d636902afc249957a32697199d516c1
Episode Renamer <eprenamer@email.com>
2018-06-13 00:34:48 -0700

    Make all dialog boxes have cancel buttons as well as ok buttons

--- src/main/org/tvrenamer/model/SWTMessageBoxType.java
+++ src/main/org/tvrenamer/model/SWTMessageBoxType.java
@@ -3,12 +3,12 @@ package org.tvrenamer.model;
 import static org.eclipse.swt.SWT.*;
 
 public enum SWTMessageBoxType {
-    DLG_ERR(ICON_ERROR),
+    DLG_ERR(ICON_ERROR|OK|CANCEL),
     // DLG_INFO(ICON_INFORMATION),
     // DLG_QUES(ICON_QUESTION),
-    DLG_WARN(ICON_WARNING),
+    DLG_WARN(ICON_WARNING|OK|CANCEL),
     // DLG_WRKG(ICON_WORKING),
-    DLG_OK(OK);
+    DLG_OK(OK|CANCEL);
 
     private final int swtIconValue;
     SWTMessageBoxType(int swtIconValue) {
