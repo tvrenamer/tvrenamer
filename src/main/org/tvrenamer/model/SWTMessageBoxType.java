591d698a342444c375dd3d7ebc7ebdb92f98055a
John Valente <johnv02139@yahoo.com>
2018-06-13 00:31:14 -0700

    In SWTMessageBoxType, import static org.eclipse.swt.SWT.*

--- src/main/org/tvrenamer/model/SWTMessageBoxType.java
+++ src/main/org/tvrenamer/model/SWTMessageBoxType.java
@@ -1,14 +1,14 @@
 package org.tvrenamer.model;
 
-import org.eclipse.swt.SWT;
+import static org.eclipse.swt.SWT.*;
 
 public enum SWTMessageBoxType {
-    DLG_ERR(SWT.ICON_ERROR),
-    // DLG_INFO(SWT.ICON_INFORMATION),
-    // DLG_QUES(SWT.ICON_QUESTION),
-    DLG_WARN(SWT.ICON_WARNING),
-    // DLG_WRKG(SWT.ICON_WORKING),
-    DLG_OK(SWT.OK);
+    DLG_ERR(ICON_ERROR),
+    // DLG_INFO(ICON_INFORMATION),
+    // DLG_QUES(ICON_QUESTION),
+    DLG_WARN(ICON_WARNING),
+    // DLG_WRKG(ICON_WORKING),
+    DLG_OK(OK);
 
     private final int swtIconValue;
     SWTMessageBoxType(int swtIconValue) {
