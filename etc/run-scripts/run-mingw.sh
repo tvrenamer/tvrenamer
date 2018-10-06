2034fb1705c40b503cc0db7b964bf86fef35ced1
John Valente <johnv02139@yahoo.com>
2018-10-06 00:02:18 -0700

    Update run scripts
    Recently, I created run-ubuntu.sh, basing it off of the existing scripts.  But in so doing, I found there was stuff that could be done better in the existing scripts.  Update them too, based on experience using them.
    
    In order for the Java files, in development (i.e., running from class files rather than a jar) to find their resources, it seems the user.dir (working directory) needs to be a particular place.  So, allow the script to be run from any working directory, but for actually launching the JVM, make sure to cd into the right directory first.

--- etc/run-scripts/run-mingw.sh
+++ etc/run-scripts/run-mingw.sh
@@ -7,24 +7,17 @@
 # files in the "out" directory.  (If you don't enable that option, we
 # assume it's because you've already run ant.)
 
-# I wrote this to be run from "git bash" for Windows, which is mingw32.
-# It has specific functionality to use Windows paths, and to choose the
-# right SWT jarfile.  To run on a different platform, the script would
-# need to be edited slightly.  To try to make sure you do that, it actually
-# doesn't run on any other platforms.  But again, trivial to change that.
+# This script is written specifically for "git bash" for Windows, which is
+# based on mingw32.
 
-# This script automatically redirects stdout into a hard-coded location.
-# It would be very easy to edit the script if you don't like that.
-
-# If anyone wants to run anywhere else, make it work
+# There are other scripts in this directory for other platforms.  If anyone
+# wants to run on a platform which currently lacks a script, edit one of
+# the scripts, and make it work!
 if [ "$MSYSTEM" != "MINGW32" ]
 then
   echo "Script only tested on MinGW 6.1 and 6.2.  Edit it for your platform."
   exit 1
 fi
-
-# local libraries -- hard-coded.  The first one is platform-specific.
-loclibs="swt-win64-4.3.jar commons-codec-1.4.jar xstream-1.4.9.jar xmlpull-1.1.3.1.jar xpp3_min-1.1.4c.jar okhttp-3.8.0.jar okio-1.13.0.jar"
 
 # Other Windows Bourne shells, like Cygwin, provide specific functionality
 # for going between Unix-style and Windows-style paths.  I don't find any
@@ -42,26 +35,46 @@ unixize ()
   echo $1 | sed 's,^\([a-zA-Z]\):,/\1,' | tr '\\' '/'
 }
 
+# libraries -- hard-coded.  The first one is platform-specific.
+loclibs="swt-win64-4.3.jar commons-codec-1.4.jar xstream-1.4.9.jar xmlpull-1.1.3.1.jar xpp3_min-1.1.4c.jar okhttp-3.8.0.jar okio-1.13.0.jar"
+
 usage ()
 {
-  echo "Error: unrecognized argument $1"
   echo "$0 [ -build ]"
   exit 3
 }
 
-# The script should be runnable from anywhere.  Stash the current location
-# to come back to it.
-startdir=`pwd`
+dobuild=0
 
+# Make sure we weren't given too many arguments
+if [ -n "$2" ]
+then
+  echo "Error: too many arguments"
+  usage
+fi
+
+# Parse the argument if there is one
+if [ -n "$1" ]
+then
+  if [ "$1" = "-build" ]
+  then
+    shift
+    dobuild=1
+  else
+    echo "Error: unrecognized argument $1"
+    usage
+  fi
+fi
+
+# If the configuration is in the older style, transform it.
+# This actually is also done by the UserPreferences class, but simpler to
+# take care of it here, beforehand.
 userhome=${HOME}
 if [ -n "${USERPROFILE}" ]
 then
   userhome=`unixize ${USERPROFILE}`
 fi
 
-# If the configuration is in the older style, transform it.
-# This actually is also done by the UserPreferences class, but simpler to
-# take care of it here, beforehand.
 if [ ! -d ${userhome}/.tvrenamer ]
 then
   if [ -f ${userhome}/.tvrenamer ]
@@ -72,28 +85,38 @@ then
   fi
 fi
 
-# Figure out where the script lives.  That tells us the project's root directory.
+# The script should be runnable from anywhere.  But the JVM needs to be
+# launched from the top level of the project directory.  (Otherwise it
+# doesn't find the necessary resources.  This could presumably be fixed,
+# but it doesn't seem worth the trouble.  It's easy enough to just cd
+# before launching the Java process.
+#
+# Stash the current directory in case we need to come back to it.
+startdir=${PWD}
+
+# Figure out where the script lives.  We "know" where it is relative to
+# the project root, so we can get to the project root by starting at
+# the script.
 proj=`dirname $0`
 cd ${proj}/../..
-pdir=`pwd`
-proj=`windowsize $pdir`
+pdir=${PWD}
 
-if [ -n "$1" ]
+# Now we're in the project's top level directory.  We can run ant, if
+# so requested.
+if [ "${dobuild}" = "1" ]
 then
-  if [ "$1" = "-build" ]
-  then
-    shift
-    ant compile || exit 2
-  fi
+  echo "building"
+  shift
+  ant compile || exit 2
 fi
 
-# Return to where we started
-cd $startdir
+# Could return to where we started, but then resources are not found
+# cd $startdir
 
 # Library files are checked in here
 loclibdir=${pdir}/jars/main
 
-CLASSPATH=${proj}/out
+CLASSPATH=`windowsize ${pdir}/out`
 for lib in ${loclibs}
 do
   CLASSPATH=${CLASSPATH}';'`windowsize ${loclibdir}/${lib}`
--- etc/run-scripts/run-osx.sh
+++ etc/run-scripts/run-osx.sh
@@ -7,10 +7,9 @@
 # files in the "out" directory.  (If you don't enable that option, we
 # assume it's because you've already run ant.)
 
-# This script automatically redirects stdout into a hard-coded location.
-# It would be very easy to edit the script if you don't like that.
-
-# If anyone wants to run anywhere else, make it work
+# There are other scripts in this directory for other platforms.  If anyone
+# wants to run on a platform which currently lacks a script, edit one of
+# the scripts, and make it work!
 if [ "`uname`" != "Darwin" ]
 then
   echo "Script only tested on Darwin.  Edit it for your platform."
@@ -22,20 +21,37 @@ loclibs="swt-osx64-4.3.jar commons-codec-1.4.jar xstream-1.4.9.jar xmlpull-1.1.3
 
 usage ()
 {
-  echo "Error: unrecognized argument $1"
   echo "$0 [ -build ]"
   exit 3
 }
 
-# The script should be runnable from anywhere.  Stash the current location
-# to come back to it.
-startdir=`pwd`
+dobuild=0
 
-userhome=${HOME}
+# Make sure we weren't given too many arguments
+if [ -n "$2" ]
+then
+  echo "Error: too many arguments"
+  usage
+fi
+
+# Parse the argument if there is one
+if [ -n "$1" ]
+then
+  if [ "$1" = "-build" ]
+  then
+    shift
+    dobuild=1
+  else
+    echo "Error: unrecognized argument $1"
+    usage
+  fi
+fi
 
 # If the configuration is in the older style, transform it.
 # This actually is also done by the UserPreferences class, but simpler to
 # take care of it here, beforehand.
+userhome=${HOME}
+
 if [ ! -d ${userhome}/.tvrenamer ]
 then
   if [ -f ${userhome}/.tvrenamer ]
@@ -46,28 +62,38 @@ then
   fi
 fi
 
-# Figure out where the script lives.  That tells us the project's root directory.
+# The script should be runnable from anywhere.  But the JVM needs to be
+# launched from the top level of the project directory.  (Otherwise it
+# doesn't find the necessary resources.  This could presumably be fixed,
+# but it doesn't seem worth the trouble.  It's easy enough to just cd
+# before launching the Java process.
+#
+# Stash the current directory in case we need to come back to it.
+startdir=${PWD}
+
+# Figure out where the script lives.  We "know" where it is relative to
+# the project root, so we can get to the project root by starting at
+# the script.
 proj=`dirname $0`
 cd ${proj}/../..
-pdir=`pwd`
-proj=$pdir
+pdir=${PWD}
 
-if [ -n "$1" ]
+# Now we're in the project's top level directory.  We can run ant, if
+# so requested.
+if [ "${dobuild}" = "1" ]
 then
-  if [ "$1" = "-build" ]
-  then
-    shift
-    ant compile || exit 2
-  fi
+  echo "building"
+  shift
+  ant compile || exit 2
 fi
 
-# Return to where we started
-cd $startdir
+# Could return to where we started, but then resources are not found
+# cd $startdir
 
 # Library files are checked in here
 loclibdir=${pdir}/jars/main
 
-CLASSPATH=${proj}/out
+CLASSPATH=${pdir}/out
 for lib in ${loclibs}
 do
   CLASSPATH=${CLASSPATH}':'${loclibdir}/${lib}
