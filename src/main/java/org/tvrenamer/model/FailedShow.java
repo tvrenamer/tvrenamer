9b21daff97223020b9cee720075782dfafccbfbf
John Valente <johnv02139@yahoo.com>
2018-10-13 16:39:05 -0700

    Add Javadoc to FailedShow

--- src/main/java/org/tvrenamer/model/FailedShow.java
+++ src/main/java/org/tvrenamer/model/FailedShow.java
@@ -7,6 +7,19 @@ public class FailedShow extends ShowOption {
     private final TVRenamerIOException err;
     private final boolean didTimeout;
 
+    /**
+     * Create a FailedShow
+     *
+     * A FailedShow is a stand-in object that we use when we do not have any
+     * better options.  It contains enough information to know what we were
+     * looking for, and give us some idea what went wrong.
+     *
+     * @param name
+     *   the name of the show we were looking for (and did not find)
+     * @param err
+     *   an exception, if one occurred while trying to look for the show;
+     *   certainly may be null
+     */
     public FailedShow(String name, TVRenamerIOException err) {
         super(null, name);
         this.err = err;
@@ -21,6 +34,12 @@ public class FailedShow extends ShowOption {
         this.didTimeout = timeout;
     }
 
+    /**
+     * Return whether the failure to look up the show was due to a timeout
+     * trying to download data from the provider.
+     *
+     * @return true if we timed out trying to look up the show; false otherwise
+     */
     public boolean isTimeout() {
         return didTimeout;
     }
