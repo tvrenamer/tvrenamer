349c393301cc149ce48adda21e4751f087f5ffdd
John Valente <johnv02139@yahoo.com>
2018-10-13 02:43:07 -0700

    Notice when provider times out
    Previously, if we tried to download information from thetvdb.com, and it is down and doesn't respond, we gave a failure message that was no different than if the show was not found.  Try to notice when there is a SocketTimeoutException, and give a different replacement text that says that we timed out.

--- src/main/java/org/tvrenamer/model/FailedShow.java
+++ src/main/java/org/tvrenamer/model/FailedShow.java
@@ -1,12 +1,28 @@
 package org.tvrenamer.model;
 
+import java.net.SocketTimeoutException;
+
 public class FailedShow extends ShowOption {
 
     private final TVRenamerIOException err;
+    private final boolean didTimeout;
 
     public FailedShow(String name, TVRenamerIOException err) {
         super(null, name);
         this.err = err;
+        boolean timeout = false;
+        Throwable exception = err;
+        while (exception != null) {
+            if (exception instanceof SocketTimeoutException) {
+                timeout = true;
+                break;
+            }
+        }
+        this.didTimeout = timeout;
+    }
+
+    public boolean isTimeout() {
+        return didTimeout;
     }
 
     @Override
