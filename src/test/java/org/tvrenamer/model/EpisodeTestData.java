c54b2898782dcabb719d781cab14442c6af4b2ac
John Valente <johnv02139@yahoo.com>
2018-10-14 13:22:11 -0700

    Add method to EpisodeTestData to create a FileEpisode
    New instance method to create and return a FileEpisode, without using any external resources (no calling the provider, no parsing the filename) based on the data in the EpisodeTestData object.  Will optionally create an actual file on disk, if given a directory in which to create it.

--- src/test/java/org/tvrenamer/model/EpisodeTestData.java
+++ src/test/java/org/tvrenamer/model/EpisodeTestData.java
@@ -1,5 +1,9 @@
 package org.tvrenamer.model;
 
+import org.tvrenamer.controller.TestUtils;
+
+import java.nio.file.Path;
+
 public class EpisodeTestData {
     private static final String EMPTY_STRING = "";
     private static final String DEFAULT_SEPARATOR = ".";
@@ -386,6 +390,97 @@ public class EpisodeTestData {
         } else {
             inputFilename = builder.inputFilename;
         }
+    }
+
+    /**
+     * Create a FileEpisode based on the data in this object.  Note this doesn't
+     * use a lot of what the real program does; it doesn't fetch anything from the
+     * Internet (or even from a cache), and it doesn't use listeners.  And, of
+     * course, it doesn't use the UI, which is intimately tied to moving files in
+     * the real program.  But it does try to simulate the process.
+     *
+     * This method takes a directory Path as an argument.  It will try to create
+     * a file that represents this object, in the directory, if a directory is
+     * given.  If the caller doesn't want an actual file created, just pass null
+     * as the directory.
+     *
+     * We create a FileEpisode from the "inputFilename" (which, unfortunately, is a
+     * bit misnamed, in that it may well be a relative path string, and is not just
+     * limited to being the filename part).  A FileEpisode initially is basically
+     * just a shell.  We then fill in information about the filename.  (In the real
+     * program, the parser in FilenameParser is used for this.)
+     *
+     * Then, once we know the part of the filename that we think represents the
+     * show name, we find the actual show name, and create a Show object to
+     * represent it.  We store the mapping between the query string and the Show
+     * object in ShowStore.  (In the real program, we send the query string to the
+     * TVDB, it responds with options in XML, which we parse, choose the best
+     * match, and use to create the Show object.)
+     *
+     * Once we have the Show object, we add the episodes.  Here, we're creating a
+     * single episode, but the Show API always expects an array.  (In the real
+     * program, we get the episodes by querying The TVDB with the show ID, and
+     * parsing the XML into an array of EpisodeInfo objects.)  We create a
+     * one-element array and stick the EpisodeInfo into it, and add that to the
+     * Show.
+     *
+     * Finally, we set the status of the FileEpisode to tell it we're finished
+     * downloading all the episodes its show needs to know about, which enables
+     * getReplacementText to give us the filename to use.  (If it didn't think we
+     * were finished adding episodes, it would instead return a placeholder text.)
+     *
+     * NOTE: this does no real error checking.  It is trivial to create an instance
+     * of EpisodeTestData that does not contain real values for some of the data
+     * required here, in which case, calling this method will be unpredictable.
+     * It is assumed the caller will have created the EpisodeTestData with enough
+     * information for this to work.
+     *
+     * @param directory
+     *   if the caller wants an actual file created, this specifies the directory
+     *   to create it in; can be null
+     * @return a FileEpisode that corresponds to the information in this object
+     *
+     */
+    public FileEpisode createFileEpisode(final Path directory) {
+        String relativeFilepath = this.inputFilename;
+
+        if (directory != null) {
+            boolean success = TestUtils.createFile(directory, relativeFilepath);
+            if (!success) {
+                return null;
+            }
+            relativeFilepath = directory.resolve(relativeFilepath).toString();
+        }
+
+        // Note, passing a String, not the actual Path.  The normal FileEpisode
+        // constructor takes a Path, and then immediately invokes the
+        // FilenameParser.  Although our filename should be parsable, we don't
+        // want to use the FilenameParser; we want to specify to the FileEpisode
+        // exactly what the components of the filename are.  The String constructor
+        // is used for this.
+        FileEpisode episode = new FileEpisode(relativeFilepath);
+
+        // This part would normally be done by the FilenameParser
+        episode.setFilenameShow(this.filenameShow);
+        episode.setEpisodePlacement(this.seasonNumString, this.episodeNumString);
+        episode.setFilenameResolution(this.episodeResolution);
+
+        // This would normally be done by getting the options from the provider.
+        Show show = ShowStore.getOrAddShow(this.filenameShow, this.properShowName);
+        episode.setEpisodeShow(show);
+
+        // This would be done by getting the listings from the provider.
+        EpisodeInfo info = new EpisodeInfo.Builder()
+            .episodeId(this.episodeId)
+            .seasonNumber(this.seasonNumString)
+            .episodeNumber(this.episodeNumString)
+            .episodeName(this.episodeTitle)
+            .build();
+        show.addOneEpisode(info);
+        show.indexEpisodesBySeason();
+        episode.listingsComplete();
+
+        return episode;
     }
 
     @Override
